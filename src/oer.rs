902a1050d877f593f9bcafc08a7fd78cbcb17566
Evan Schwartz <evan.mark.schwartz@gmail.com>
2017-07-07 01:00:26 +0200

    feat: deserialize ilp payments

--- src/oer.rs
+++ src/oer.rs
@@ -1,7 +1,8 @@
-use std::io::Error;
-use byteorder::{BigEndian, WriteBytesExt};
+use std::io::{Cursor, Error};
+use byteorder::{BigEndian, WriteBytesExt, ReadBytesExt};
 
-const MSB: u8 = 0x80;
+const HIGH_BIT: u8 = 0x80;
+const LOWER_SEVEN_BITS: u8 = 0x7f;
 
 // TODO maybe just further extend the byteorder traits and export that as OER, since it implements
 // all the functions except for the var octet strings
@@ -14,13 +15,37 @@ pub fn write_var_octet_string(data: &mut Vec<u8>, string: &Vec<u8>) -> Result<()
     } else {
         let bit_length_of_length = format!("{:b}", length).chars().count();
         let length_of_length = {bit_length_of_length as f32 / 8.0}.ceil() as u8;
-        data.write_u8(MSB | length_of_length)?;
+        data.write_u8(HIGH_BIT | length_of_length)?;
         data.write_uint::<BigEndian>(length as u64, length_of_length as usize)?;
     }
     data.extend(string);
     Ok(())
 }
 
+pub fn read_var_octet_string(data: &[u8]) -> Result<&[u8], Error> {
+    let mut reader = Cursor::new(data);
+    let length: u8 = reader.read_u8()?;
+
+    if length == 0 {
+        let empty: [u8; 0] = [];
+        return Ok(&empty.to_owned());
+    }
+
+    let actual_length: usize = if length & HIGH_BIT != 0 {
+        println!("got here");
+        let length_prefix_length = length & LOWER_SEVEN_BITS;
+        reader.read_uint::<BigEndian>(length_prefix_length as usize)? as usize
+        // TODO check for canonical length
+    } else {
+        length as usize
+    };
+
+
+    let pos = reader.position() as usize;
+
+    // TODO handle if the length is too long
+    Ok(&data[pos..(pos + actual_length)])
+}
 
 #[cfg(test)]
 mod tests {
@@ -46,5 +71,26 @@ mod tests {
         expected.extend(larger_string);
         assert_eq!(larger.len(), 259);
         assert_eq!(larger, expected);
+    }
+
+    #[test]
+    fn it_reads_octet_string() {
+        assert_eq!(
+            read_var_octet_string(&vec![0]).unwrap(),
+            &[]);
+
+        assert_eq!(
+            read_var_octet_string(&vec![0x01, 0xb0]).unwrap(),
+            &[0xb0]);
+
+        let mut larger = vec![0x82, 0x01, 0x00];
+        let mut larger_string: Vec<u8> = Vec::with_capacity(256 as usize);
+        for _ in 0..256 {
+            larger_string.push(0xb0);
+        }
+        larger.extend(&larger_string);
+        assert_eq!(
+            read_var_octet_string(&larger).unwrap(),
+            &larger_string[..]);
     }
 }
--- src/packet.rs
+++ src/packet.rs
@@ -1,10 +1,22 @@
 use oer;
-use std::io::Error;
-use byteorder::{BigEndian, WriteBytesExt};
+use std::io::{Cursor, Error, ErrorKind};
+use byteorder::{BigEndian, WriteBytesExt, ReadBytesExt};
 
+#[derive(Debug, PartialEq)]
 #[repr(u8)]
 enum PacketType {
     IlpPayment = 1,
+    Unknown
+}
+impl From<u8> for PacketType {
+    fn from(type_int: u8) -> Self {
+        match type_int {
+            1 => PacketType::IlpPayment,
+            _ => PacketType::Unknown
+        }
+
+    }
+
 }
 
 pub enum IlpPacket {
@@ -13,6 +25,7 @@ pub enum IlpPacket {
 // TODO add IlpPacket trait with serialization functions
 
 // TODO add JSON serialization for debugging and CLI
+#[derive(Debug, PartialEq, Clone)]
 pub struct IlpPayment {
     amount: u64,
     account: String,
@@ -20,9 +33,27 @@ pub struct IlpPayment {
 }
 
 impl IlpPayment {
-    // TODO add validation
+    pub fn from_bytes(bytes: &[u8]) -> Result<IlpPayment, Error> {
+        let (packet_type, contents) = deserialize_envelope(bytes)?;
+        if packet_type != PacketType::IlpPayment {
+            return Err(Error::new(ErrorKind::Other, format!("attempted to deserialize packet type {:?} as IlpPayment", packet_type)));
+        }
 
-    pub fn serialize(&self) -> Result<Vec<u8>, Error> {
+        let mut reader = Cursor::new(contents);
+        let amount = reader.read_u64::<BigEndian>()?;
+        let account_bytes = oer::read_var_octet_string(&contents[reader.position() as usize..])?;
+        let account = String::from_utf8(account_bytes.to_vec()).map_err(|_| Error::new(ErrorKind::Other, "account not utf8"))?;
+        let start_of_data_pos = reader.position() + account.len() as u64 + 1;
+        let data_bytes = oer::read_var_octet_string(&contents[start_of_data_pos as usize..])?;
+        let data = data_bytes.to_vec();
+        Ok(IlpPayment {
+            amount: amount,
+            account: account,
+            data: data,
+        })
+    }
+
+    pub fn to_bytes(&self) -> Result<Vec<u8>, Error> {
         let mut payment_vec: Vec<u8> = Vec::new();
         payment_vec.write_u64::<BigEndian>(self.amount)?;
         oer::write_var_octet_string(&mut payment_vec, &self.account.to_string().into_bytes())?;
@@ -39,6 +70,15 @@ fn serialize_envelope(packet_type: PacketType, contents: &Vec<u8>) -> Result<Vec
     packet.write_u8(packet_type as u8)?;
     oer::write_var_octet_string(&mut packet, contents)?;
     Ok(packet)
+}
+
+fn deserialize_envelope(bytes: &[u8]) -> Result<(PacketType, &[u8]), Error> {
+    let mut reader = Cursor::new(bytes);
+    let packet_type = PacketType::from(reader.read_u8()?);
+    let pos = reader.position() as usize;
+    let slice: &[u8] = &reader.get_ref()[pos..];
+    Ok((packet_type,
+       oer::read_var_octet_string(slice)?))
 }
 
 #[cfg(test)]
@@ -66,7 +106,7 @@ mod tests {
             data: vec![]
         };
         assert_eq!(
-            without_data.serialize().unwrap(),
+            without_data.to_bytes().unwrap(),
             hex_to_bytes("0118000000000000006b0d6578616d706c652e616c6963650000"),
             "without data");
 
@@ -76,8 +116,31 @@ mod tests {
             data: base64::decode("ZZZZ").unwrap()
         };
         assert_eq!(
-            with_data.serialize().unwrap(),
+            with_data.to_bytes().unwrap(),
             hex_to_bytes("011900000000000000640b6578616d706c652e626f620365965900"),
+            "with data");
+    }
+
+    #[test]
+    fn it_deserializes_ilp_payments() {
+        let without_data = IlpPayment {
+            amount: 107,
+            account: "example.alice".to_string(),
+            data: vec![]
+        };
+        assert_eq!(
+            IlpPayment::from_bytes(&hex_to_bytes("0118000000000000006b0d6578616d706c652e616c6963650000")[..]).unwrap(),
+            without_data,
+            "without data");
+
+        let with_data = IlpPayment {
+            amount: 100,
+            account: "example.bob".to_string(),
+            data: base64::decode("ZZZZ").unwrap()
+        };
+        assert_eq!(
+            IlpPayment::from_bytes(&hex_to_bytes("011900000000000000640b6578616d706c652e626f620365965900")[..]).unwrap(),
+            with_data,
             "with data");
     }
 }
