0cadf9a34b00588d80bd45f59e4a772833d61d5f
Evan Schwartz <evan.mark.schwartz@gmail.com>
2018-09-01 18:12:31 -0400

    feat: make Plugin a Stream

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,19 @@
 extern crate ilp;
 extern crate tokio;
 extern crate bytes;
+extern crate futures;
 
 use tokio::prelude::*;
-use ilp::plugin_btp::{connect_async, PluginBtp, Plugin};
+use ilp::plugin_btp::{connect_async, PluginBtp, PluginItem};
 use bytes::Bytes;
+use futures::{Stream, Sink};
 
 
 fn main() {
   let future = connect_async("ws://localhost:7768")
-    // .and_then(|plugin| {
-    //   plugin.send_data(Bytes::from_slice(&vec![1, 2, 3]))
-    // })
-    // .and_then(|(response, plugin)| {
-    .and_then(|plugin| {
-      plugin.send_money(1234 as u64)
-    })
-    .and_then(|plugin| {
-      Ok(())
-    });
+  .and_then(|plugin| {
+    plugin.send(PluginItem::Data(Bytes::from(vec![0, 1, 2])))
+    .map(|_| { () })
+  });
   tokio::runtime::run(future);
 }
