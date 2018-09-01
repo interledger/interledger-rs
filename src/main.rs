e2cde91aa0524c736a4fdc884d529dd628af4d19
Evan Schwartz <evan.mark.schwartz@gmail.com>
2018-09-01 14:41:45 -0400

    feat: use Bytes crate instead of vec or slice

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
 extern crate ilp;
 extern crate tokio;
+extern crate bytes;
 
 use tokio::prelude::*;
 use ilp::plugin_btp::{connect_async, PluginBtp, Plugin};
+use bytes::Bytes;
+
 
 fn main() {
   let future = connect_async("ws://localhost:7768")
+    // .and_then(|plugin| {
+    //   plugin.send_data(Bytes::from_slice(&vec![1, 2, 3]))
+    // })
+    // .and_then(|(response, plugin)| {
     .and_then(|plugin| {
-      plugin.send_data(&vec![1, 2, 3])
+      plugin.send_money(1234 as u64)
     })
-    .and_then(|(response, plugin)| {
-      println!("Got response {:?}", response);
+    .and_then(|plugin| {
       Ok(())
     });
   tokio::runtime::run(future);
