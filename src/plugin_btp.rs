d1bb129387956a3b2149e88b6cb56d4f146ed0ea
Evan Schwartz <evan.mark.schwartz@gmail.com>
2018-09-01 18:12:31 -0400

    feat: make Plugin a Stream

--- src/plugin_btp.rs
+++ src/plugin_btp.rs
@@ -1,52 +1,95 @@
 use std::error::Error as StdError;
-use btp_packet::{BtpMessage, BtpResponse, Serializable};
+use btp_packet::{BtpMessage, BtpResponse, Serializable, ProtocolData, ContentType};
 use tokio_tungstenite::{connect_async as connect_websocket, WebSocketStream, MaybeTlsStream};
-use tungstenite::{Error as WebSocketError};
+use tungstenite::{Error as WebSocketError, Message as WebSocketMessage};
 use tokio_tcp::TcpStream;
-use futures::Future;
+use futures::{Future, Async, AsyncSink};
 use futures::future::{ok};
 use futures::stream::{Stream, SplitSink, SplitStream};
+use futures::sink::{Sink};
 use url::{Url, ParseError};
-use bytes::{Bytes};
+use bytes::{Bytes, BytesMut};
 
-pub trait Plugin {
-  fn send_data(self, data: Bytes) -> Box<Future<Item = (Bytes, Self), Error = ()>>;
-  fn send_money(self, amount: u64) -> Box<Future<Item = Self, Error = ()>>;
+pub enum PluginItem {
+  Data(Bytes),
+  Money(u64),
 }
 
 pub struct PluginBtp {
   server: Url,
-  ws_sink: SplitSink<WebSocketStream<MaybeTlsStream<TcpStream>>>,
-  ws_stream: SplitStream<WebSocketStream<MaybeTlsStream<TcpStream>>>,
+  ws: WebSocketStream<MaybeTlsStream<TcpStream>>,
+  request_id: u32,
 }
 
-impl Plugin for PluginBtp {
-  fn send_data(self, data: Bytes) -> Box<Future<Item = (Bytes, Self), Error = ()>> {
-    println!("Sending data {:?}", data.clone());
-    let future = ok(()).and_then(|_| {
-      Ok((Bytes::from_slice(&vec![]), self))
-    });
-    Box::new(future)
+impl Stream for PluginBtp {
+  type Item = PluginItem;
+  type Error = Box<StdError>;
+
+  fn poll(&mut self) -> Result<Async<Option<Self::Item>>, Self::Error> {
+    match self.ws.poll() {
+      Ok(Async::Ready(Some(ws_message))) => {
+        // TODO parse the websocket message
+        println!("Got response {:?}", ws_message);
+        Ok(Async::Ready(Some(PluginItem::Money(1))))
+      },
+      Ok(Async::Ready(None)) => Ok(Async::Ready(None)),
+      Ok(Async::NotReady) => Ok(Async::NotReady),
+      Err(e) => Err(Box::from(e))
+    }
+  }
+}
+
+impl Sink for PluginBtp {
+  type SinkItem = PluginItem;
+  type SinkError = ();
+
+  fn start_send(&mut self, item: PluginItem) -> Result<AsyncSink<Self::SinkItem>, Self::SinkError> {
+    match item {
+      PluginItem::Data(data) => {
+        let btp = BtpMessage {
+          request_id: {
+            self.request_id += 1;
+            self.request_id
+          },
+          protocol_data: vec![ProtocolData {
+            protocol_name: String::from("ilp"),
+            content_type: ContentType::ApplicationOctetStream,
+            data: data.to_vec()
+          }]
+        };
+        let ws_message = WebSocketMessage::Binary(btp.to_bytes().unwrap());
+        self.ws.start_send(ws_message)
+          .map(|result| {
+            match result {
+              AsyncSink::Ready => AsyncSink::Ready,
+              AsyncSink::NotReady(_) => AsyncSink::NotReady(PluginItem::Data(data))
+            }
+          })
+          .map_err(|err| {
+            println!("Error sending {}", err);
+          })
+      },
+      PluginItem::Money(amount) => {
+        Err(())
+      }
+    }
   }
 
-  fn send_money(self, amount: u64) -> Box<Future<Item = Self, Error = ()>> {
-    println!("Sending money {}", amount);
-    let future = ok(()).and_then(|_| {
-      Ok(self)
-    });
-    Box::new(future)
+  fn poll_complete(&mut self) -> Result<Async<()>, Self::SinkError> {
+    self.ws.poll_complete().map_err(|e| {
+      println!("Polling error {}", e);
+    })
   }
 }
 
 pub fn connect_async(server: &str) -> Box<Future<Item = PluginBtp, Error = ()> + 'static + Send> {
   let server = Url::parse(server).unwrap();
-  let future = connect_websocket(server.clone()).and_then(move |(ws_stream, _ )| {
+  let future = connect_websocket(server.clone()).and_then(move |(ws, _ )| {
     println!("Connected to ${}", &server);
-    let (ws_sink, ws_stream) = ws_stream.split();
     Ok(PluginBtp {
       server,
-      ws_sink,
-      ws_stream,
+      ws,
+      request_id: 0,
     })
   }).map_err(|e| {
     println!("Error connecting: {}", e);
