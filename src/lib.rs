3c2506955b903b5537bb99a874f3f7d96fbd018e
Evan Schwartz <evan.mark.schwartz@gmail.com>
2018-09-02 21:42:16 -0400

    feat: basic ILP packet parsing stream

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ extern crate hex;
 #[macro_use]
 extern crate lazy_static;
 extern crate num_bigint;
+#[macro_use]
 extern crate futures;
 extern crate tokio_tungstenite;
 extern crate tokio_tcp;
@@ -17,6 +18,7 @@ extern crate bytes;
 pub mod errors;
 pub mod oer;
 pub mod ilp_packet;
+pub mod ilp_packet_stream;
 pub mod btp_packet;
 pub mod plugin_btp;
 mod util;
