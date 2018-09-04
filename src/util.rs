c8c202c1cf06c96102ace4673c749f98f63b8d44
Evan Schwartz <evan.mark.schwartz@gmail.com>
2018-09-03 22:31:48 -0400

    feat: ILP Fulfillment checker stream

--- src/util.rs
+++ src/util.rs
@@ -1,4 +1,6 @@
 use errors::ParseError;
+use ilp_packet::IlpPacket;
+use btp_packet::BtpPacket;
 
 pub trait Serializable<T> {
     fn from_bytes(bytes: &[u8]) -> Result<T, ParseError>;
@@ -7,7 +9,7 @@ pub trait Serializable<T> {
 }
 
 pub struct OutgoingRequestIdGenerator {
-    priv next_id: u32
+    next_id: u32
 }
 
 impl OutgoingRequestIdGenerator {
@@ -21,4 +23,16 @@ impl OutgoingRequestIdGenerator {
         self.next_id += 1;
         self.next_id
     }
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum IlpOrBtpPacket {
+  Ilp(u32, IlpPacket),
+  Btp(BtpPacket),
+}
+
+impl From<BtpPacket> for IlpOrBtpPacket {
+  fn from(btp: BtpPacket) -> Self {
+    IlpOrBtpPacket::Btp(btp)
+  }
 }
