efea975ac389f9f9e477dd3c2905ec2ed17f3413
Evan Schwartz <evan.mark.schwartz@gmail.com>
2018-09-06 00:51:22 -0400

    feat: BTP request ID checker

--- src/lib.rs
+++ src/lib.rs
@@ -21,7 +21,8 @@ pub mod oer;
 pub mod ilp_packet;
 pub mod ilp_packet_stream;
 pub mod btp_packet;
-pub mod plugin_btp;
+pub mod btp_packet_stream;
+pub mod btp_request_id_checker;
 pub mod ilp_fulfillment_checker;
 mod util;
 
