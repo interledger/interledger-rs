307e02fd894d11b98dccdeafa00dfed08c1d9036
Evan Schwartz <evan.mark.schwartz@gmail.com>
2018-09-15 10:55:23 -0400

    feat(stream): reject invalid prepare packets
    taking inspiration from how futures::stream::Forward works
    based on the recommendation of @carllerche

--- src/stream/packet_stream.rs
+++ src/stream/packet_stream.rs
@@ -1,119 +1,235 @@
 use super::crypto::{fulfillment_to_condition, generate_fulfillment};
 use super::StreamPacket;
 use bytes::Bytes;
+use futures::stream::Fuse;
 use futures::{Async, AsyncSink, Poll, Sink, StartSend, Stream};
 use ilp::{IlpPacket, IlpReject};
 
-pub struct StreamPacketStream<S> {
+pub struct StreamPacketStream<T: Stream, U> {
   shared_secret: Bytes,
-  inner: S,
+  sink: Option<U>,
+  stream: Option<Fuse<T>>,
+  buffered: Option<T::Item>,
 }
 
-impl<S> StreamPacketStream<S>
+impl<T, U> StreamPacketStream<T, U>
 where
-  S:
-    Stream<Item = (u32, IlpPacket), Error = ()> + Sink<SinkItem = (u32, IlpPacket), SinkError = ()>,
+  T: Stream<Item = (u32, IlpPacket), Error = ()>,
+  U: Sink<SinkItem = (u32, IlpPacket), SinkError = ()>,
 {
-  pub fn new(shared_secret: Bytes, stream: S) -> Self {
+  pub fn new(shared_secret: Bytes, stream: T, sink: U) -> Self {
     StreamPacketStream {
       shared_secret,
-      inner: stream,
+      stream: Some(stream.fuse()),
+      sink: Some(sink),
+      buffered: None,
     }
+  }
+
+  fn sink_mut(&mut self) -> Option<&mut U> {
+    self.sink.as_mut()
+  }
+
+  fn stream_mut(&mut self) -> Option<&mut T> {
+    self.stream.as_mut().map(|x| x.get_mut())
+  }
+
+  fn try_start_send(&mut self, item: T::Item) -> Poll<(), U::SinkError> {
+    debug_assert!(self.buffered.is_none());
+    if let AsyncSink::NotReady(item) = self
+      .sink_mut()
+      .take()
+      .expect("Attempted to poll after completion")
+      .start_send(item)?
+    {
+      self.buffered = Some(item);
+      return Ok(Async::NotReady);
+    }
+    Ok(Async::Ready(()))
   }
 }
 
-impl<S> Stream for StreamPacketStream<S>
+impl<T, U> Stream for StreamPacketStream<T, U>
 where
-  S:
-    Stream<Item = (u32, IlpPacket), Error = ()> + Sink<SinkItem = (u32, IlpPacket), SinkError = ()>,
+  T: Stream<Item = (u32, IlpPacket), Error = ()>,
+  U: Sink<SinkItem = (u32, IlpPacket), SinkError = ()>,
 {
-  type Item = (u32, IlpPacket, StreamPacket);
+  type Item = (u32, IlpPacket, Option<StreamPacket>);
   type Error = ();
 
   fn poll(&mut self) -> Poll<Option<Self::Item>, Self::Error> {
-    if let Some((request_id, packet)) = try_ready!(self.inner.poll()) {
-      let parse_result = match packet {
-        IlpPacket::Prepare(ref packet) => {
-          let fulfillment = generate_fulfillment(&self.shared_secret[..], &packet.data[..]);
-          let condition = fulfillment_to_condition(&fulfillment);
-          if condition != packet.execution_condition {
-            warn!("Got ILP packet where the condition does not match the one we generate from the data. Expected: {:?}, packet: {:?}", &condition[..], packet);
-            // TODO reject packet
-            return Ok(Async::NotReady);
-          }
-          StreamPacket::from_encrypted(&self.shared_secret[..], &packet.data[..])
-        }
-        IlpPacket::Fulfill(ref packet) => {
-          StreamPacket::from_encrypted(&self.shared_secret[..], &packet.data[..])
-        }
-        IlpPacket::Reject(ref packet) => {
-          StreamPacket::from_encrypted(&self.shared_secret[..], &packet.data[..])
-        }
-        IlpPacket::Unknown => {
-          warn!("Got ILP packet with no data: {:?}", packet);
-          // TODO reject packet
-          // self.inner.start_send((request_id, IlpPacket::Reject(IlpReject::new("F06", "", "", Bytes::new()))));
+    // If we've got an item buffered already, we need to write it to the
+    // sink before we can do anything else
+    if let Some(item) = self.buffered.take() {
+      self.try_start_send(item)?;
+      return Ok(Async::NotReady);
+    }
+
+    let next_item = try_ready!(
+      self
+        .stream_mut()
+        .take()
+        .expect("Attempted to poll after completion")
+        .poll()
+    );
+
+    if next_item.is_none() {
+      return Ok(Async::Ready(None));
+    }
+
+    let (request_id, packet) = next_item.unwrap();
+
+    // Try parsing the iLP packet data as a STREAM packet
+    match packet {
+      IlpPacket::Prepare(packet) => {
+        // Check that the condition matches what we regenerate
+        let fulfillment = generate_fulfillment(&self.shared_secret[..], &packet.data[..]);
+        let condition = fulfillment_to_condition(&fulfillment);
+        if condition != packet.execution_condition {
+          // Reject the packet
+          warn!("Got ILP packet where the condition does not match the one we generate from the data. Expected: {:?}, packet: {:?}", &condition[..], packet);
+          let reject = (
+            request_id,
+            IlpPacket::Reject(IlpReject::new("F02", "", "", Bytes::new())),
+          );
+          self.try_start_send(reject)?;
           return Ok(Async::NotReady);
         }
-      };
-      if let Ok(stream_packet) = parse_result {
-        Ok(Async::Ready(Some((request_id, packet, stream_packet))))
-      } else {
-        warn!("Got ILP packet with data we cannot parse: {:?}", packet);
-        // TODO reject packet
-        // self.inner.start_send((request_id, IlpPacket::Reject(IlpReject::new("F06", "", "", Bytes::new()))))
-        //   .map_err(|_| {
-        //     warn!("Error rejecting packet {}", request_id);
-        //   });
+
+        // Check if we can decrypt and parse the STREAM packet
+        if let Ok(stream_packet) =
+          StreamPacket::from_encrypted(&self.shared_secret[..], &packet.data[..])
+        {
+          Ok(Async::Ready(Some((
+            request_id,
+            IlpPacket::Prepare(packet),
+            Some(stream_packet),
+          ))))
+        } else {
+          // Reject the packet
+          warn!("Got ILP packet with data we cannot parse: {:?}", packet);
+          let reject = (
+            request_id,
+            IlpPacket::Reject(IlpReject::new("F06", "", "", Bytes::new())),
+          );
+          self.try_start_send(reject)?;
+          Ok(Async::NotReady)
+        }
+      }
+      IlpPacket::Fulfill(packet) => {
+        // Check if we can decrypt and parse the STREAM packet
+        if let Ok(stream_packet) =
+          StreamPacket::from_encrypted(&self.shared_secret[..], &packet.data[..])
+        {
+          Ok(Async::Ready(Some((
+            request_id,
+            IlpPacket::Fulfill(packet),
+            Some(stream_packet),
+          ))))
+        } else {
+          warn!(
+            "Got ILP Fulfill for request: {} with no data attached: {:?}",
+            request_id, packet
+          );
+          Ok(Async::Ready(Some((
+            request_id,
+            IlpPacket::Fulfill(packet),
+            None,
+          ))))
+        }
+      }
+      IlpPacket::Reject(packet) => {
+        // Check if we can decrypt and parse the STREAM packet
+        if let Ok(stream_packet) =
+          StreamPacket::from_encrypted(&self.shared_secret[..], &packet.data[..])
+        {
+          Ok(Async::Ready(Some((
+            request_id,
+            IlpPacket::Reject(packet),
+            Some(stream_packet),
+          ))))
+        } else {
+          Ok(Async::Ready(Some((
+            request_id,
+            IlpPacket::Reject(packet),
+            None,
+          ))))
+        }
+      }
+      IlpPacket::Unknown => {
+        warn!("Got ILP packet with no data: {:?}", packet);
+        let reject = (
+          request_id,
+          IlpPacket::Reject(IlpReject::new("F06", "", "", Bytes::new())),
+        );
+        self.try_start_send(reject)?;
         Ok(Async::NotReady)
       }
-    } else {
-      Ok(Async::Ready(None))
     }
   }
 }
 
-impl<S> Sink for StreamPacketStream<S>
+impl<T, U> Sink for StreamPacketStream<T, U>
 where
-  S: Sink<SinkItem = (u32, IlpPacket), SinkError = ()>,
+  T: Stream<Item = (u32, IlpPacket), Error = ()>,
+  U: Sink<SinkItem = (u32, IlpPacket), SinkError = ()>,
 {
-  type SinkItem = (u32, IlpPacket, StreamPacket);
+  type SinkItem = (u32, IlpPacket, Option<StreamPacket>);
   type SinkError = ();
 
   fn start_send(&mut self, item: Self::SinkItem) -> StartSend<Self::SinkItem, Self::SinkError> {
     let (request_id, packet, stream_packet) = item;
-    let encrypted = stream_packet.to_encrypted(&self.shared_secret[..]).unwrap();
-    let packet = match packet {
-      IlpPacket::Prepare(mut packet) => {
-        let fulfillment = generate_fulfillment(&self.shared_secret[..], encrypted.as_slice());
-        let condition = fulfillment_to_condition(&fulfillment);
-        packet.data = Bytes::from(encrypted);
-        packet.execution_condition = condition;
-        IlpPacket::Prepare(packet)
+
+    // TODO error if the ILP Packet data isn't empty
+
+    // Replace the packet data with the encrypted STREAM packet
+    let (packet, stream_packet) = if let Some(stream_packet) = stream_packet {
+      let encrypted = stream_packet
+        .to_encrypted(&self.shared_secret[..])
+        .unwrap();
+
+      match packet {
+        IlpPacket::Prepare(mut packet) => {
+          let fulfillment = generate_fulfillment(&self.shared_secret[..], encrypted.as_slice());
+          let condition = fulfillment_to_condition(&fulfillment);
+          packet.data = Bytes::from(encrypted);
+          packet.execution_condition = condition;
+          (IlpPacket::Prepare(packet), Some(stream_packet))
+        }
+        IlpPacket::Fulfill(mut packet) => {
+          let fulfillment = generate_fulfillment(&self.shared_secret[..], encrypted.as_slice());
+          packet.data = Bytes::from(encrypted);
+          packet.fulfillment = fulfillment;
+          (IlpPacket::Fulfill(packet), Some(stream_packet))
+        }
+        IlpPacket::Reject(mut packet) => {
+          packet.data = Bytes::from(encrypted);
+          (IlpPacket::Reject(packet), Some(stream_packet))
+        }
+        IlpPacket::Unknown => return Err(()),
       }
-      IlpPacket::Fulfill(mut packet) => {
-        let fulfillment = generate_fulfillment(&self.shared_secret[..], encrypted.as_slice());
-        packet.data = Bytes::from(encrypted);
-        packet.fulfillment = fulfillment;
-        IlpPacket::Fulfill(packet)
-      }
-      IlpPacket::Reject(mut packet) => {
-        packet.data = Bytes::from(encrypted);
-        IlpPacket::Reject(packet)
-      }
-      IlpPacket::Unknown => return Err(()),
+    } else {
+      (packet, stream_packet)
     };
 
     self
-      .inner
+      .sink_mut()
+      .take()
+      .expect("Attempted to send after completion")
       .start_send((request_id, packet))
       .map(|result| match result {
         AsyncSink::Ready => AsyncSink::Ready,
-        AsyncSink::NotReady((request_id, packet)) => AsyncSink::NotReady((request_id, packet, stream_packet)),
+        AsyncSink::NotReady((request_id, packet)) => {
+          AsyncSink::NotReady((request_id, packet, stream_packet))
+        }
       })
   }
 
   fn poll_complete(&mut self) -> Result<Async<()>, Self::SinkError> {
-    self.inner.poll_complete()
+    self
+      .sink_mut()
+      .take()
+      .expect("Attempted to send after completion")
+      .poll_complete()
   }
 }
