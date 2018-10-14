344af613056154d23cb29545f0009eacf70b03ab
Evan Schwartz <evan.mark.schwartz@gmail.com>
2018-10-14 11:26:21 -0400

    feat(cli): sending spsp payments

--- src/main.rs
+++ src/main.rs
@@ -14,52 +14,100 @@ pub fn main() {
     .subcommand(
       SubCommand::with_name("spsp")
         .about("Client and Server for the Simple Payment Setup Protocol (SPSP)")
-        .subcommand(
+        .subcommands(vec![
           SubCommand::with_name("server")
             .about("Run an SPSP Server that automatically accepts incoming money")
-            .arg(Arg::with_name("port").short("p").default_value("3000")),
-        ),
+            .arg(
+              Arg::with_name("port")
+                .long("port")
+                .short("p")
+                .takes_value(true)
+                .default_value("3000"),
+            ),
+          SubCommand::with_name("pay")
+            .about("Send an SPSP payment")
+            .args(&[
+              Arg::with_name("receiver")
+                .long("receiver")
+                .short("r")
+                .takes_value(true)
+                .required(true),
+              Arg::with_name("amount")
+                .long("amount")
+                .short("a")
+                .takes_value(true)
+                .required(true),
+            ]),
+        ]),
     );
-  let matches = app.clone().get_matches();
 
-  if let Some(matches) = matches.subcommand_matches("spsp") {
+  match app.clone().get_matches().subcommand() {
+    ("spsp", Some(matches)) => match matches.subcommand() {
+      ("server", Some(matches)) => {
+        let port = value_t!(matches, "port", u16).expect("Invalid port");
+        run_spsp_server(port);
+      }
+      ("pay", Some(matches)) => {
+        let receiver = value_t!(matches, "receiver", String).expect("Receiver is required");
+        let amount = value_t!(matches, "amount", u64).expect("Invalid amount");
+        send_spsp_payment(receiver, amount);
+      }
+      _ => app.print_help().unwrap(),
+    },
+    _ => app.print_help().unwrap(),
+  }
+}
 
-    // Run SPSP Server
-    if let Some(matches) = matches.subcommand_matches("server") {
-      let port = value_t!(matches, "port", u16).expect("Invalid port");
-      let run = ilp::plugin::btp::connect_to_moneyd()
+fn send_spsp_payment(receiver: String, amount: u64) {
+  let run = ilp::plugin::btp::connect_to_moneyd()
+    .map_err(|err| {
+      println!("Error connecting to BTP server: {:?}", err);
+    })
+    .and_then(move |plugin| {
+      ilp::spsp::pay(plugin, &receiver, amount.clone())
         .map_err(|err| {
-          println!("Error connecting to BTP server: {:?}", err);
+          println!("Error sending SPSP payment: {:?}", err);
         })
-        .and_then(move |plugin| {
-          ilp::spsp::listen_with_random_secret(plugin, port)
-            .map_err(|err| {
-              println!("Error listening: {}", err);
-            })
-            .and_then(move |listener| {
-              let handle_connections = listener.for_each(|(id, connection)| {
-                let conn_id = Arc::new(id);
-                let handle_streams = connection.for_each(move |stream| {
-                  let conn_id = Arc::clone(&conn_id);
-                  let handle_money = stream.money.for_each(move |amount| {
-                    println!("Connection {} got incoming money: {}", conn_id, amount);
-                    Ok(())
-                  });
-                  tokio::spawn(handle_money);
-                  Ok(())
-                });
-                tokio::spawn(handle_streams);
+        .and_then(move |delivered| {
+          println!(
+            "Sent: {}, delivered: {} (in the receiver's units)",
+            amount, delivered
+          );
+          Ok(())
+        })
+    });
+  tokio::run(run);
+}
+
+fn run_spsp_server(port: u16) {
+  let run = ilp::plugin::btp::connect_to_moneyd()
+    .map_err(|err| {
+      println!("Error connecting to BTP server: {:?}", err);
+    })
+    .and_then(move |plugin| {
+      ilp::spsp::listen_with_random_secret(plugin, port)
+        .map_err(|err| {
+          println!("Error listening: {}", err);
+        })
+        .and_then(move |listener| {
+          let handle_connections = listener.for_each(|(id, connection)| {
+            let conn_id = Arc::new(id);
+            let handle_streams = connection.for_each(move |stream| {
+              let conn_id = Arc::clone(&conn_id);
+              let handle_money = stream.money.for_each(move |amount| {
+                println!("Connection {} got incoming money: {}", conn_id, amount);
                 Ok(())
               });
-              tokio::spawn(handle_connections);
-              println!("Listening for SPSP connections on port: {}", port);
+              tokio::spawn(handle_money);
               Ok(())
-            })
-        });
-      tokio::run(run);
-      return;
-    }
-  }
-
-  app.print_help().unwrap();
+            });
+            tokio::spawn(handle_streams);
+            Ok(())
+          });
+          tokio::spawn(handle_connections);
+          println!("Listening for SPSP connections on port: {}", port);
+          Ok(())
+        })
+    });
+  tokio::run(run);
 }
