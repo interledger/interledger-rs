eb50aee3adacff837a7c875971e0182c3633a28d
Evan Schwartz <evan.mark.schwartz@gmail.com>
2019-02-13 11:03:41 -0500

    feat: http client service

--- crates/interledger-http/src/store.rs
+++ crates/interledger-http/src/store.rs
@@ -6,13 +6,14 @@ pub struct HttpDetails {
     pub auth_header: String,
 }
 
-pub trait HttpStore {
+// TODO do we need all of these constraints?
+pub trait HttpStore: Clone + Send + Sync + 'static {
     fn get_account_from_authorization(
         &self,
         auth_header: &str,
-    ) -> Box<Future<Item = AccountId, Error = ()>>;
+    ) -> Box<Future<Item = AccountId, Error = ()> + Send>;
     fn get_http_details_for_account(
         &self,
         account: AccountId,
-    ) -> Box<Future<Item = HttpDetails, Error = ()>>;
+    ) -> Box<Future<Item = HttpDetails, Error = ()> + Send>;
 }
