8a5fc9731f4167105d63ebf70abe136aeb5412f6
Evan Schwartz <evan.mark.schwartz@gmail.com>
2019-02-22 18:39:43 -0500

    feat: add ILDCP client and server

---
+++ crates/interledger-ildcp/src/packet.rs
@@ -0,0 +1,134 @@
+use byteorder::{BigEndian, ReadBytesExt};
+use bytes::{BufMut, Bytes, BytesMut};
+use interledger_packet::{
+  oer::{predict_var_octet_string, BufOerExt, MutBufOerExt},
+  Fulfill, FulfillBuilder, ParseError, Prepare, PrepareBuilder, Reject,
+};
+use std::ops::Add;
+use std::time::{Duration, SystemTime};
+
+static ILDCP_DESTINATION: &'static [u8] = b"peer.config";
+static PEER_PROTOCOL_FULFILLMENT: [u8; 32] = [0; 32];
+static PEER_PROTOCOL_CONDITION: [u8; 32] = [
+  102, 104, 122, 173, 248, 98, 189, 119, 108, 143, 193, 139, 142, 159, 142, 32, 8, 151, 20, 133,
+  110, 226, 51, 179, 144, 42, 89, 29, 13, 95, 41, 37,
+];
+const ASSET_SCALE_LEN: usize = 1;
+
+lazy_static! {
+  static ref PEER_PROTOCOL_EXPIRY_DURATION: Duration = Duration::from_secs(60);
+}
+
+pub fn is_ildcp_request(prepare: &Prepare) -> bool {
+  prepare.execution_condition() == PEER_PROTOCOL_CONDITION && prepare.destination() == ILDCP_DESTINATION
+}
+
+#[derive(Debug, Default)]
+pub struct IldcpRequest {}
+
+impl IldcpRequest {
+  pub fn new() -> Self {
+    IldcpRequest {}
+  }
+
+  pub fn to_prepare(&self) -> Prepare {
+    PrepareBuilder {
+      destination: ILDCP_DESTINATION,
+      amount: 0,
+      execution_condition: &PEER_PROTOCOL_CONDITION,
+      expires_at: SystemTime::now().add(*PEER_PROTOCOL_EXPIRY_DURATION),
+      data: &[],
+    }
+    .build()
+  }
+}
+
+impl From<IldcpRequest> for Prepare {
+  fn from(request: IldcpRequest) -> Self {
+    request.to_prepare()
+  }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct IldcpResponse {
+  buffer: Bytes,
+  asset_scale: u8,
+  asset_code_offset: usize,
+}
+
+impl From<IldcpResponse> for Bytes {
+  fn from(response: IldcpResponse) -> Self {
+    response.buffer
+  }
+}
+
+impl From<IldcpResponse> for Fulfill {
+  fn from(response: IldcpResponse) -> Self {
+    FulfillBuilder {
+      fulfillment: &PEER_PROTOCOL_FULFILLMENT,
+      data: &response.buffer[..],
+    }
+    .build()
+  }
+}
+
+impl IldcpResponse {
+  pub fn try_from(buffer: Bytes) -> Result<Self, ParseError> {
+    let mut reader = &buffer[..];
+    let buffer_len = reader.len();
+
+    reader.skip_var_octet_string()?;
+    let asset_scale = reader.read_u8()?;
+
+    let asset_code_offset = buffer_len - reader.len();
+    reader.skip_var_octet_string()?;
+
+    Ok(IldcpResponse {
+      buffer,
+      asset_scale,
+      asset_code_offset,
+    })
+  }
+
+  pub fn client_address(&self) -> &[u8] {
+    (&self.buffer[..]).peek_var_octet_string().unwrap()
+  }
+
+  pub fn asset_scale(&self) -> u8 {
+    self.asset_scale
+  }
+
+  pub fn asset_code(&self) -> &[u8] {
+    (&self.buffer[self.asset_code_offset..])
+      .peek_var_octet_string()
+      .unwrap()
+  }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct IldcpResponseBuilder<'a> {
+  pub client_address: &'a [u8],
+  pub asset_scale: u8,
+  pub asset_code: &'a str,
+}
+
+impl<'a> IldcpResponseBuilder<'a> {
+  pub fn build(&self) -> IldcpResponse {
+    let address_size = predict_var_octet_string(self.client_address.len());
+    let asset_code_size = predict_var_octet_string(self.asset_code.len());
+    let buf_size = ASSET_SCALE_LEN + address_size + asset_code_size;
+    let mut buffer = BytesMut::with_capacity(buf_size);
+
+    buffer.put_var_octet_string_length(self.client_address.len());
+    buffer.put_slice(self.client_address);
+    buffer.put_u8(self.asset_scale);
+    buffer.put_var_octet_string_length(self.asset_code.len());
+    buffer.put_slice(self.asset_code.as_bytes());
+
+    IldcpResponse {
+      buffer: buffer.freeze(),
+      asset_scale: self.asset_scale,
+      asset_code_offset: address_size + ASSET_SCALE_LEN,
+    }
+  }
+}
--- crates/interledger-store-memory/src/lib.rs
+++ crates/interledger-store-memory/src/lib.rs
@@ -1,104 +1,118 @@
-extern crate bytes;
-extern crate futures;
-extern crate hashbrown;
-extern crate interledger_http;
-extern crate interledger_router;
-extern crate interledger_service;
-
 use bytes::Bytes;
 use futures::{future::result, Future};
 use hashbrown::HashMap;
 use interledger_http::{HttpDetails, HttpStore};
+use interledger_ildcp::{AccountDetails, IldcpStore};
 use interledger_router::RouterStore;
 use interledger_service::AccountId;
 use std::sync::Arc;
 
 pub struct Account {
-    ilp_address: Bytes,
-    additional_routes: Vec<Bytes>,
-    asset_code: String,
-    asset_scale: u8,
-    http_endpoint: Option<String>,
-    http_incoming_authorization: Option<String>,
-    http_outgoing_authorization: Option<String>,
+  ilp_address: Bytes,
+  additional_routes: Vec<Bytes>,
+  asset_code: String,
+  asset_scale: u8,
+  http_endpoint: Option<String>,
+  http_incoming_authorization: Option<String>,
+  http_outgoing_authorization: Option<String>,
 }
 
 #[derive(Clone)]
 pub struct InMemoryStore {
-    accounts: Arc<HashMap<u64, Account>>,
-    routing_table: Arc<Vec<(Bytes, AccountId)>>,
+  accounts: Arc<HashMap<u64, Account>>,
+  routing_table: Arc<Vec<(Bytes, AccountId)>>,
 }
 
 impl InMemoryStore {
-    pub fn new(accounts: HashMap<u64, Account>) -> Self {
-        let accounts = Arc::new(accounts);
-        let mut routing_table = Vec::with_capacity(
-            accounts
-                .values()
-                .map(|account| 1usize + account.additional_routes.len())
-                .sum(),
-        );
-        for (account_id, account) in accounts.iter() {
-            routing_table.push((account.ilp_address.clone(), *account_id));
+  pub fn new(accounts: HashMap<u64, Account>) -> Self {
+    let accounts = Arc::new(accounts);
+    let mut routing_table = Vec::with_capacity(
+      accounts
+        .values()
+        .map(|account| 1usize + account.additional_routes.len())
+        .sum(),
+    );
+    for (account_id, account) in accounts.iter() {
+      routing_table.push((account.ilp_address.clone(), *account_id));
 
-            for route in account.additional_routes.iter() {
-                routing_table.push((route.clone(), *account_id));
-            }
-        }
-        Self {
-            accounts,
-            routing_table: Arc::new(routing_table),
-        }
+      for route in account.additional_routes.iter() {
+        routing_table.push((route.clone(), *account_id));
+      }
     }
+    Self {
+      accounts,
+      routing_table: Arc::new(routing_table),
+    }
+  }
 }
 
 impl HttpStore for InMemoryStore {
-    fn get_account_from_authorization(
-        &self,
-        auth_header: &str,
-    ) -> Box<Future<Item = AccountId, Error = ()> + Send> {
-        Box::new(result(
-            self.accounts
-                .iter()
-                .find(|(_account_id, account)| {
-                    if let Some(ref header) = account.http_incoming_authorization {
-                        if header == auth_header {
-                            return true;
-                        }
-                    }
-                    false
-                })
-                .ok_or(())
-                .map(|(account_id, _account)| *account_id),
-        ))
-    }
+  fn get_account_from_authorization(
+    &self,
+    auth_header: &str,
+  ) -> Box<Future<Item = AccountId, Error = ()> + Send> {
+    Box::new(result(
+      self
+        .accounts
+        .iter()
+        .find(|(_account_id, account)| {
+          if let Some(ref header) = account.http_incoming_authorization {
+            if header == auth_header {
+              return true;
+            }
+          }
+          false
+        })
+        .ok_or(())
+        .map(|(account_id, _account)| *account_id),
+    ))
+  }
 
-    fn get_http_details_for_account(
-        &self,
-        account_id: AccountId,
-    ) -> Box<Future<Item = HttpDetails, Error = ()> + Send> {
-        Box::new(result(self.accounts.get(&account_id).ok_or(()).and_then(
-            |account| {
-                if let Some(url) = &account.http_endpoint {
-                    let auth_header = if let Some(auth) = &account.http_outgoing_authorization {
-                        auth.to_string()
-                    } else {
-                        String::new()
-                    };
-                    Ok(HttpDetails {
-                        url: url.to_string(),
-                        auth_header,
-                    })
-                } else {
-                    Err(())
-                }
-            },
-        )))
-    }
+  fn get_http_details_for_account(
+    &self,
+    account_id: AccountId,
+  ) -> Box<Future<Item = HttpDetails, Error = ()> + Send> {
+    Box::new(result(self.accounts.get(&account_id).ok_or(()).and_then(
+      |account| {
+        if let Some(url) = &account.http_endpoint {
+          let auth_header = if let Some(auth) = &account.http_outgoing_authorization {
+            auth.to_string()
+          } else {
+            String::new()
+          };
+          Ok(HttpDetails {
+            url: url.to_string(),
+            auth_header,
+          })
+        } else {
+          Err(())
+        }
+      },
+    )))
+  }
 }
 
 impl RouterStore for InMemoryStore {
-    fn get_routing_table(&self) -> Arc<Vec<(Bytes, AccountId)>> {
-        self.routing_table.clone()
-    }
+  fn get_routing_table(&self) -> Arc<Vec<(Bytes, AccountId)>> {
+    self.routing_table.clone()
+  }
+}
+
+impl IldcpStore for InMemoryStore {
+  fn get_account_details(
+    &self,
+    account_id: AccountId,
+  ) -> Box<Future<Item = AccountDetails, Error = ()> + Send> {
+    Box::new(result(
+      self
+        .accounts
+        .get(&account_id)
+        .map(|account| AccountDetails {
+          client_address: account.ilp_address.clone(),
+          asset_code: account.asset_code.clone(),
+          asset_scale: account.asset_scale,
+        })
+        .ok_or(()),
+    ))
+  }
 }
