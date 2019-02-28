67b9e6e2641eb9a2d6219435952fbc1dcc61d8bb
Evan Schwartz <evan.mark.schwartz@gmail.com>
2019-02-28 00:55:01 -0500

    feat: add rejecter service
    intended to be the last service in the chain

--- crates/interledger-service-util/src/lib.rs
+++ crates/interledger-service-util/src/lib.rs
@@ -1,5 +1,10 @@
+#[macro_use]
+extern crate log;
+
 mod max_packet_amount;
+mod rejecter;
 mod validator;
 
 pub use self::max_packet_amount::{MaxPacketAmountService, MaxPacketAmountStore};
+pub use self::rejecter::RejecterService;
 pub use self::validator::ValidatorService;
