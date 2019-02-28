dcebd1f55ba90244bd19989646a2426098e6d973
Evan Schwartz <evan.mark.schwartz@gmail.com>
2019-02-28 00:54:24 -0500

    feat(router): allow use of empty string as catch-all prefix

--- crates/interledger-router/src/lib.rs
+++ crates/interledger-router/src/lib.rs
@@ -1,8 +1,5 @@
-extern crate bytes;
-extern crate futures;
-extern crate interledger_packet;
-extern crate interledger_service;
-extern crate parking_lot;
+#[macro_use]
+extern crate log;
 
 mod router;
 mod store;
