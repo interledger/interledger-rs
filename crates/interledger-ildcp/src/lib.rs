cc52821a22217af1a8f343cb85ee2d56841791dd
Evan Schwartz <evan.mark.schwartz@gmail.com>
2019-02-28 00:52:28 -0500

    chore(ildcp): add logging

--- crates/interledger-ildcp/src/lib.rs
+++ crates/interledger-ildcp/src/lib.rs
@@ -1,5 +1,7 @@
 #[macro_use]
 extern crate lazy_static;
+#[macro_use]
+extern crate log;
 
 mod client;
 mod packet;
@@ -7,6 +9,6 @@ mod server;
 mod store;
 
 pub use client::get_ildcp_info;
+pub use packet::*;
 pub use server::IldcpService;
 pub use store::{AccountDetails, IldcpStore};
-pub use packet::*;
