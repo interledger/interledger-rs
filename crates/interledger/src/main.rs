54085c18ed80af06756643fffaa67954bcea217a
Evan Schwartz <evan.mark.schwartz@gmail.com>
2019-03-01 13:19:12 -0500

    feat(cli): enable use of ILP-over-HTTP

--- crates/interledger/src/main.rs
+++ crates/interledger/src/main.rs
@@ -4,12 +4,11 @@ extern crate clap;
 
 use base64;
 use clap::{App, Arg, SubCommand};
-use interledger::send_spsp_payment;
+use interledger::*;
 use ring::rand::{SecureRandom, SystemRandom};
 
 fn random_token() -> String {
-    // TODO should this be shorter?
-    let mut bytes: [u8; 32] = [0; 32];
+    let mut bytes: [u8; 18] = [0; 18];
     SystemRandom::new().fill(&mut bytes).unwrap();
     base64::encode_config(&bytes, base64::URL_SAFE_NO_PAD)
 }
@@ -17,7 +16,7 @@ fn random_token() -> String {
 pub fn main() {
     env_logger::init();
 
-    let moneyd_url = format!(
+    let moneyd_uri = format!(
         "btp+ws://{}:{}@localhost:7768",
         random_token(),
         random_token()
@@ -52,6 +51,14 @@ pub fn main() {
                     SubCommand::with_name("pay")
                         .about("Send an SPSP payment")
                         .args(&[
+                            Arg::with_name("btp_server")
+                                .long("btp_server")
+                                .default_value(&moneyd_uri)
+                                .help("URI of a moneyd or BTP Server to pay from"),
+                            Arg::with_name("http_server")
+                                .long("http_server")
+                                .takes_value(true)
+                                .help("HTTP URL of the connector to pay from"),
                             Arg::with_name("receiver")
                                 .long("receiver")
                                 .short("r")
@@ -63,11 +70,7 @@ pub fn main() {
                                 .short("a")
                                 .takes_value(true)
                                 .required(true)
-                                .help("Amount to send, denominated in the BTP Server's units"),
-                            Arg::with_name("btp_server")
-                                .long("btp_server")
-                                .default_value(&moneyd_url)
-                                .help("URI of a moneyd or BTP Server to pay from"),
+                                .help("Amount to send, denominated in the connector's units"),
                             Arg::with_name("quiet")
                                 .long("quiet")
                                 .help("Suppress log output"),
@@ -86,12 +89,18 @@ pub fn main() {
             //     run_spsp_server(&btp_server, port, notification_endpoint, quiet);
             // }
             ("pay", Some(matches)) => {
-                let btp_server =
-                    value_t!(matches, "btp_server", String).expect("BTP Server URL is required");
                 let receiver = value_t!(matches, "receiver", String).expect("Receiver is required");
                 let amount = value_t!(matches, "amount", u64).expect("Invalid amount");
                 let quiet = matches.is_present("quiet");
-                send_spsp_payment(&btp_server, &receiver, amount, quiet);
+
+                // Check for http_server first because btp_server has the default value of connecting to moneyd
+                if let Ok(http_server) = value_t!(matches, "http_server", String) {
+                    send_spsp_payment_http(&http_server, &receiver, amount, quiet)
+                } else if let Ok(btp_server) = value_t!(matches, "btp_server", String) {
+                    send_spsp_payment_btp(&btp_server, &receiver, amount, quiet);
+                } else {
+                    panic!("Must specify either btp_server or http_server");
+                }
             }
             _ => app.print_help().unwrap(),
         },
