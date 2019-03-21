445fbf602e6a2e62e8f293427181498e2347f04e
Evan Schwartz <evan.mark.schwartz@gmail.com>
2019-03-21 19:38:34 -0400

    feat: add Docker container w/ node and settlement engine

---
+++ Dockerfile
@@ -0,0 +1,32 @@
+FROM rust:1.33 as rust
+
+WORKDIR /usr/src
+COPY ./Cargo.toml /usr/src/Cargo.toml
+COPY ./crates /usr/src/crates
+
+# Build Interledger node
+# TODO build release
+RUN cargo build --package interledger
+
+FROM node:11-slim as node
+
+# Install Redis
+RUN apt-get update \
+    && apt-get -y install redis-server
+
+# Build settlement engine
+COPY ./settlement-engines /usr/src/settlement-engines
+WORKDIR /usr/src/settlement-engines/xrp
+RUN npm run build
+WORKDIR /usr/src
+
+# Copy Interledger binary
+COPY --from=rust /usr/src/target/debug/interledger /usr/src/target/debug/interledger
+
+# Expose ports for HTTP and BTP
+EXPOSE 7768
+EXPOSE 7770
+
+COPY ./run-node.js ./run-node.js
+
+CMD ["node", "./run-node.js"]
---
+++ run-node.js
@@ -0,0 +1,64 @@
+#!/usr/bin/env node
+
+const { spawn } = require('child_process')
+
+async function run() {
+    const xrpAddress = process.env.XRP_ADDRESS
+    const xrpSecret = process.env.XRP_SECRET
+    const adminToken = process.env.ADMIN_TOKEN
+    const ilpAddress = process.env.ILP_ADDRESS || 'private.local.node'
+
+    if (!xrpAddress || !xrpSecret || !adminToken) {
+        console.error('Must provide XRP_ADDRESS, XRP_SECRET, and ADMIN_TOKEN')
+        process.exit(1)
+    }
+
+    console.log('Starting redis-server')
+    const redis = spawn('redis-server', {
+        stdio: 'inherit'
+    })
+    redis.on('error', (err) => console.error('Redis error:', err))
+    redis.on('exit', (code, signal) => console.error(`Redis exited with code: ${code} and signal: ${signal}`))
+
+    console.log('Starting XRP settlement engine')
+    const settlementEngine = spawn('./settlement-engines/xrp/build/cli.js', [`--address=${xrpAddress}`, `--secret=${xrpSecret}`], {
+        env: {
+            DEBUG: process.env.DEBUG
+        },
+        stdio: 'inherit'
+    })
+    settlementEngine.on('error', (err) => console.error('Settlement engine error:', err))
+    settlementEngine.on('exit', (code, signal) => console.error(`Settlement engine exited with code: ${code} and signal: ${signal}`))
+
+    console.log('Creating admin account')
+    const createAccount = spawn('./target/debug/interledger', [
+        'node',
+        'accounts',
+        'add',
+        `--ilp_address=${ilpAddress}`,
+        `--xrp_address=${xrpAddress}`,
+        `--http_incoming_token=${adminToken}`,
+        '--asset_code=XRP',
+        '--asset_scale=9',
+        '--admin'
+    ], {
+            stdio: 'inherit',
+            env: {
+                RUST_LOG: process.env.RUST_LOG
+            }
+        })
+    createAccount.on('error', (err) => console.error('Error creating account:', err))
+
+    console.log('Launching Interledger node')
+    const node = spawn('./target/debug/interledger', ['node'], {
+        stdio: 'inherit',
+        env: {
+            RUST_LOG: process.env.RUST_LOG,
+        }
+    })
+    node.on('error', (err) => console.error('Interledger node error:', err))
+    node.on('exit', (code, signal) => console.error(`Interledger node exited with code: ${code} and signal: ${signal}`))
+}
+
+run().catch((err) => console.error(err))
+
