659b9a92b2d14a6f1b846ea9eaf677026333ab89
Evan Schwartz <evan.mark.schwartz@gmail.com>
2019-03-30 10:35:19 -0400

    feat(ccp): [WIP] add CCP routing request handler

--- crates/interledger-ccp/src/lib.rs
+++ crates/interledger-ccp/src/lib.rs
@@ -1,7 +1,14 @@
 #[macro_use]
 extern crate log;
-#[cfg(test)]
 #[macro_use]
 extern crate lazy_static;
 
+#[cfg(test)]
+mod fixtures;
 mod packet;
+mod server;
+
+pub trait RoutingAccount {
+    fn should_send_routes(&self) -> bool;
+    fn should_receive_routes(&self) -> bool;
+}
