4bdd048455e8f847a50642873c0fef8ab9e75085
Evan Schwartz <evan.mark.schwartz@gmail.com>
2019-04-04 20:31:38 -0400

    feat(docker): include localtunnel in bundle

--- run-interledger-node.js
+++ run-interledger-node.js
@@ -18,16 +18,17 @@ async function run() {
     let adminToken = process.env.ADMIN_TOKEN
     let rippled = process.env.XRP_SERVER
     let ilpAddress = process.env.ILP_ADDRESS
+    let useLocaltunnel = !process.env.DISABLE_LOCALTUNNEL
+    let localTunnelSubdomain = process.env.LOCALTUNNEL_SUBDOMAIN
     const redisDir = process.env.REDIS_DIR || '.'
 
-    let shouldCreateAdminAccount = true
+    let createAdminAccount = true
 
     // Try reading from config file or generating testnet credentials
     if (!xrpAddress) {
         let config = await loadConfig()
-        if (config) {
-            shouldCreateAdminAccount = false
-        } else {
+        createAdminAccount = false
+        if (!config) {
             config = await generateTestnetCredentials()
         }
         xrpAddress = config.xrpAddress
@@ -35,11 +36,31 @@ async function run() {
         adminToken = config.adminToken
         rippled = config.rippled
         ilpAddress = config.ilpAddress
+        localTunnelSubdomain = localTunnelSubdomain || config.localTunnelSubdomain
     }
 
     if (!xrpAddress || !xrpSecret || !adminToken || !ilpAddress) {
         console.error('Must provide XRP_ADDRESS, XRP_SECRET, ILP_ADDRESS, and ADMIN_TOKEN')
         process.exit(1)
+    }
+
+    console.log('Starting redis-server')
+    const redis = spawn('redis-server', [
+        // Use a unix socket instead of TCP
+        `--unixsocket ${REDIS_UNIX_SOCKET}`,
+        '--unixsocketperm 777',
+        // Save redis data using append-only log of commands
+        '--appendonly yes',
+        '--appendfsync everysec',
+        `--dir ${redisDir}`
+    ], {
+            stdio: 'inherit'
+        })
+    redis.on('error', (err) => console.error('Redis error:', err))
+    redis.on('exit', (code, signal) => console.error(`Redis exited with code: ${code} and signal: ${signal}`))
+
+    if (useLocaltunnel) {
+        runLocalTunnel(localTunnelSubdomain, true)
     }
 
     console.log('Starting XRP settlement engine')
@@ -58,25 +79,33 @@ async function run() {
     settlementEngine.on('error', (err) => console.error('Settlement engine error:', err))
     settlementEngine.on('exit', (code, signal) => console.error(`Settlement engine exited with code: ${code} and signal: ${signal}`))
 
-    console.log('Creating admin account')
-    const createAccount = spawn('interledger', [
-        'node',
-        'accounts',
-        'add',
-        `--redis_uri=unix:${REDIS_UNIX_SOCKET}`,
-        `--ilp_address=${ilpAddress}`,
-        `--xrp_address=${xrpAddress}`,
-        `--http_incoming_token=${adminToken}`,
-        '--asset_code=XRP',
-        '--asset_scale=9',
-        '--admin'
-    ], {
-            stdio: 'inherit',
-            env: {
-                RUST_LOG: process.env.RUST_LOG
-            }
+    if (createAdminAccount) {
+        console.log('Creating admin account')
+        const createAccount = spawn('interledger', [
+            'node',
+            'accounts',
+            'add',
+            `--redis_uri=unix:${REDIS_UNIX_SOCKET}`,
+            `--ilp_address=${ilpAddress}`,
+            `--xrp_address=${xrpAddress}`,
+            `--http_incoming_token=${adminToken}`,
+            '--asset_code=XRP',
+            '--asset_scale=9',
+            '--admin'
+        ], {
+                stdio: 'inherit',
+                env: {
+                    RUST_LOG: process.env.RUST_LOG
+                }
+            })
+        createAccount.on('error', (err) => console.error('Error creating account:', err))
+
+        // Wait for the default account to be created before launching the node
+        await new Promise((resolve) => {
+            createAccount.once('exit', resolve)
+            setTimeout(resolve, 1000)
         })
-    createAccount.on('error', (err) => console.error('Error creating account:', err))
+    }
 
     console.log('Launching Interledger node')
     const node = spawn('interledger', [
@@ -90,23 +119,46 @@ async function run() {
         })
     node.on('error', (err) => console.error('Interledger node error:', err))
     node.on('exit', (code, signal) => console.error(`Interledger node exited with code: ${code} and signal: ${signal}`))
+
+    await new Promise((resolve) => setTimeout(resolve, 200))
+
+    console.log('\n\n')
+
+    // Print instructions
+    if (!process.env.ADMIN_TOKEN) {
+        console.log(`>>> Admin API Authorization header: "Bearer ${adminToken}"\n`)
+    }
+
+    if (!process.env.LOCALTUNNEL_SUBDOMAIN) {
+        console.log(`>>> Node is accessible via: https://${localTunnelSubdomain}.localtunnel.me \n`)
+    }
+
+    if (!process.env.XRP_ADDRESS) {
+        console.log(`>>> XRP Address: ${xrpAddress} \n`)
+    }
+
+    console.log('\n')
 }
 
 run().catch((err) => console.error(err))
 
 async function loadConfig() {
     try {
-        const configFile = await promisify(fs.readFile)(CONFIG_PATH, { encoding: 'utf8' })
-        const config = JSON.parse(config)
+        console.log('Loading config...')
+        const configFile = await promisify(fs.readFile).call(null, CONFIG_PATH, { encoding: 'utf8' })
+        const config = JSON.parse(configFile)
+        console.log('Loaded config')
         return config
     } catch (err) {
+        console.log('No config loaded')
         return null
     }
 }
 
 async function generateTestnetCredentials() {
+    console.log('Generating testnet credentials...')
     const adminToken = randomBytes(20).toString('hex')
-    console.log(`Admin HTTP Bearer token is: ${adminToken}`)
+    const localTunnelSubdomain = 'ilp-node-' + randomBytes(12).toString('hex')
 
     console.log('Fetching XRP testnet credentials')
     const faucetResponse = await new Promise((resolve, reject) => {
@@ -138,13 +190,15 @@ async function generateTestnetCredentials() {
 
     // Write config file
     try {
-        await promisify(fs.writeFile)(CONFIG_PATH, JSON.stringify({
+        await promisify(fs.writeFile).call(null, CONFIG_PATH, JSON.stringify({
             xrpAddress,
             xrpSecret,
             adminToken,
             ilpAddress,
-            rippled
+            rippled,
+            localTunnelSubdomain
         }))
+        console.log('Saved config to ', CONFIG_PATH)
     } catch (err) {
         console.error("Error writing to config file. XRP Address, Secret, and admin token were not saved.", err)
     }
@@ -153,7 +207,23 @@ async function generateTestnetCredentials() {
         xrpSecret,
         rippled,
         ilpAddress,
-        adminToken
+        adminToken,
+        localTunnelSubdomain
     }
 }
 
+function runLocalTunnel(subdomain, restartOnError) {
+    console.log('Starting localtunnel to expose the local node to others')
+    const lt = spawn('lt', [
+        '--port=7770',
+        `--subdomain=${subdomain}`
+    ])
+    lt.on('error', (err) => {
+        console.error('localtunnel error:', err)
+        if (restartOnError) {
+            runLocalTunnel(subdomain, restartOnError)
+        }
+    })
+    lt.on('exit', (code, signal) => console.error(`localtunnel exited with code: ${code} and signal: ${signal}`))
+
+}
