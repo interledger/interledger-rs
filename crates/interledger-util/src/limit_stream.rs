8f9c5974362b2c18fd91dfcaf7d428646af8f507
Taiga Nakayama <dora@dora-gt.jp>
2019-05-03 01:14:13 +0900

    updated license

--- crates/interledger-util/src/limit_stream.rs
+++ crates/interledger-util/src/limit_stream.rs
@@ -1,7 +1,16 @@
 // Copyright 2019 @sentientwaffle
 //
-// Released under the MIT license
-// [https://opensource.org/licenses/mit-license.php](https://opensource.org/licenses/mit-license.php)
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 use std::error::Error;
 use std::fmt;
