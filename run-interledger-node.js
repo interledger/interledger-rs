8d4e01da86caf6945656036996686c0645286678
Evan Schwartz <evan.mark.schwartz@gmail.com>
2019-05-16 10:16:21 -0400

    chore: remove settlement engine
    will be replaced by an external settlement adapter, as discussed in https://forum.interledger.org/t/settlement-architecture/545

--- run-interledger-node.js
+++ run-interledger-node.js
@@ -5,48 +5,31 @@ const { randomBytes } = require('crypto')
 const fs = require('fs')
 const { promisify } = require('util')
 const localtunnel = require('localtunnel')
-const request = require('request-promise-native')
 
 // Have the redis-server listen on a unix socket instead of TCP because it's faster
 const REDIS_UNIX_SOCKET = '/tmp/redis.sock'
-const XRP_FAUCET_URL = 'https://faucet.altnet.rippletest.net/accounts'
-const XRP_TESTNET_URI = 'wss://s.altnet.rippletest.net:51233'
 const CONFIG_PATH = '/data/node-config.json'
 
-const BOOTSTRAP_NODES = [{
-    signupEndpoint: 'https://ilp-node-2204f3bf5901558b1ef045f6.localtunnel.me/accounts/prepaid',
-    ilpEndpoint: 'https://ilp-node-2204f3bf5901558b1ef045f6.localtunnel.me/ilp',
-    xrpAddress: 'rQ3eSt2HyozxRixDUxFanCnsw79zE1np1x',
-    ilpAddress: 'test.xrp.rQ3eSt2HyozxRixDUxFanCnsw79zE1np1x'
-}]
-
 async function run() {
-    let xrpAddress = process.env.XRP_ADDRESS
-    let xrpSecret = process.env.XRP_SECRET
     let adminToken = process.env.ADMIN_TOKEN
-    let rippled = process.env.XRP_SERVER
     let ilpAddress = process.env.ILP_ADDRESS
     let useLocaltunnel = !process.env.DISABLE_LOCALTUNNEL
     let localTunnelSubdomain = process.env.LOCALTUNNEL_SUBDOMAIN
     const redisDir = process.env.REDIS_DIR || '.'
     let nodeDomain = process.env.DOMAIN
-    const connectToBootstrapNodes = !process.env.DISABLE_BOOTSTRAP
     let nodeSecret = process.env.ILP_NODE_SECRET
 
     let createAccounts = true
 
     // Try reading from config file or generating testnet credentials
-    if (!xrpAddress) {
+    if (!adminToken) {
         let config = await loadConfig()
         if (config) {
             createAccounts = false
         } else {
             config = await generateTestnetCredentials()
         }
-        xrpAddress = config.xrpAddress
-        xrpSecret = config.xrpSecret
         adminToken = config.adminToken
-        rippled = config.rippled
         ilpAddress = config.ilpAddress
         localTunnelSubdomain = localTunnelSubdomain || config.localTunnelSubdomain
         nodeSecret = nodeSecret || config.nodeSecret
@@ -60,8 +43,8 @@ async function run() {
         }
     }
 
-    if (!xrpAddress || !xrpSecret || !adminToken || !ilpAddress) {
-        console.error('Must provide XRP_ADDRESS, XRP_SECRET, ILP_ADDRESS, and ADMIN_TOKEN')
+    if (!adminToken || !ilpAddress) {
+        console.error('Must provide ILP_ADDRESS and ADMIN_TOKEN')
         process.exit(1)
     }
 
@@ -84,125 +67,11 @@ async function run() {
         await runLocalTunnel(localTunnelSubdomain, true)
     }
 
-    console.log('Starting XRP settlement engine')
-    const settlementEngine = spawn('xrp-settlement-engine',
-        [
-            `--redis=${REDIS_UNIX_SOCKET}`,
-            `--address=${xrpAddress}`,
-            `--secret=${xrpSecret}`,
-            `--rippled=${rippled}`,
-            `--poll_interval=15000`
-        ], {
-            env: {
-                DEBUG: process.env.DEBUG
-            },
-            stdio: 'inherit'
-        })
-    settlementEngine.on('error', (err) => console.error('Settlement engine error:', err))
-    settlementEngine.on('exit', (code, signal) => console.error(`Settlement engine exited with code: ${code} and signal: ${signal}`))
-
-    if (createAccounts) {
-        console.log('Creating admin account')
-        const createAccount = spawn('interledger', [
-            'node',
-            'accounts',
-            'add',
-            `--redis_uri=unix:${REDIS_UNIX_SOCKET}`,
-            `--server_secret=${nodeSecret}`,
-            `--ilp_address=${ilpAddress}`,
-            `--xrp_address=${xrpAddress}`,
-            `--http_incoming_token=${adminToken}`,
-            '--asset_code=XRP',
-            '--asset_scale=9',
-            '--admin',
-            // TODO make this unlimited
-            '--min_balance=-1000000000'
-        ], {
-                stdio: 'inherit',
-                env: {
-                    RUST_LOG: process.env.RUST_LOG
-                }
-            })
-        createAccount.on('error', (err) => console.error('Error creating account:', err))
-
-        if (connectToBootstrapNodes) {
-            console.log('Peering with bootstrap nodes')
-            for (let node of BOOTSTRAP_NODES) {
-                const authToken = randomBytes(20).toString('hex')
-                const peerAuthToken = randomBytes(20).toString('hex')
-                node.authToken = authToken
-                node.peerAuthToken = peerAuthToken
-                const httpUrl = node.ilpEndpoint.replace('//', `//:${node.authToken}@`)
-                console.log(httpUrl)
-                const createAccount = spawn('interledger', [
-                    'node',
-                    'accounts',
-                    'add',
-                    `--redis_uri=unix:${REDIS_UNIX_SOCKET}`,
-                    `--server_secret=${nodeSecret}`,
-                    `--ilp_address=${node.ilpAddress}`,
-                    `--xrp_address=${node.xrpAddress}`,
-                    `--http_incoming_token=${node.peerAuthToken}`,
-                    `--http_url=${httpUrl}`,
-                    '--asset_code=XRP',
-                    '--asset_scale=9',
-                    '--settle_threshold=0',
-                    '--settle_to=-1000000000',
-                    '--min_balance=-10000000000',
-                    '--send_routes',
-                    '--receive_routes'
-                ], {
-                        stdio: 'inherit',
-                        env: {
-                            RUST_LOG: process.env.RUST_LOG
-                        }
-                    })
-                createAccount.on('error', (err) => console.error('Error creating peer account:', err))
-            }
-
-            // Wait to make sure the settlement engine has sent the outgoing payment
-            await new Promise((resolve) => {
-                createAccount.once('exit', resolve)
-                setTimeout(resolve, 20000)
-            })
-
-            for (let node of BOOTSTRAP_NODES) {
-                console.log(`Creating account on bootstrap node: ${node.signupEndpoint}`)
-                await request.post({
-                    uri: node.signupEndpoint,
-                    body: {
-
-                        ilp_address: ilpAddress,
-                        xrp_address: xrpAddress,
-                        asset_code: 'XRP',
-                        asset_scale: 9,
-                        http_incoming_token: node.authToken,
-                        http_outgoing_token: node.peerAuthToken,
-                        http_endpoint: nodeDomain,
-                        settleThreshold: 100000000,
-                        settleTo: 0,
-                        send_routes: true,
-                        receive_routes: true
-                    },
-                    json: true
-                })
-            }
-        } else {
-            // Wait for the default account to be created before launching the node
-            await new Promise((resolve) => {
-                createAccount.once('exit', resolve)
-                setTimeout(resolve, 1000)
-            })
-        }
-    }
-
     console.log('Launching Interledger node')
     const node = spawn('interledger', [
         'node',
         `--redis_uri=unix:${REDIS_UNIX_SOCKET}`,
         `--server_secret=${nodeSecret}`,
-        // TODO make this greater than 0
-        '--open_signups_min_balance=0'
     ], {
             stdio: 'inherit',
             env: {
@@ -223,10 +92,6 @@ async function run() {
         console.log(`>>> Admin API Authorization header: "Bearer ${adminToken}"\n`)
     }
 
-    if (!process.env.XRP_ADDRESS) {
-        console.log(`>>> XRP Address: ${xrpAddress} \n`)
-    }
-
     console.log(`>>> Try sending a test payment by sending an HTTP POST request to:
 
     ${nodeDomain}/pay
@@ -234,7 +99,7 @@ async function run() {
     with the header: "Authorization": "Bearer ${adminToken}"
     and the body:
     {
-        "receiver": "$ilp-node-2204f3bf5901558b1ef045f6.localtunnel.me",
+        "receiver": "$receiver-payment-pointer.example",
         "source_amount": 1000
     }
     `)
@@ -261,24 +126,14 @@ async function generateTestnetCredentials() {
     console.log('Generating testnet credentials...')
     const adminToken = randomBytes(20).toString('hex')
     const localTunnelSubdomain = 'ilp-node-' + randomBytes(12).toString('hex')
-
-    console.log('Fetching XRP testnet credentials')
-    const faucetResponse = await request.post(XRP_FAUCET_URL, { json: true })
-    const xrpAddress = faucetResponse.account.address
-    const xrpSecret = faucetResponse.account.secret
-    const rippled = XRP_TESTNET_URI
-    const ilpAddress = `test.xrp.${xrpAddress} `
+    const ilpAddress = `test.${randomBytes(20).toString('hex')} `
     const nodeSecret = randomBytes(32).toString('hex')
-    console.log(`Got testnet XRP address: ${xrpAddress} and secret: ${xrpSecret} `)
 
     // Write config file
     try {
         await promisify(fs.writeFile).call(null, CONFIG_PATH, JSON.stringify({
-            xrpAddress,
-            xrpSecret,
             adminToken,
             ilpAddress,
-            rippled,
             localTunnelSubdomain,
             nodeSecret
         }))
@@ -287,9 +142,6 @@ async function generateTestnetCredentials() {
         console.error("Error writing to config file. XRP Address, Secret, and admin token were not saved.", err)
     }
     return {
-        xrpAddress,
-        xrpSecret,
-        rippled,
         ilpAddress,
         adminToken,
         localTunnelSubdomain,
