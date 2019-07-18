1fdb18c3bec4e3cdfdf730efc56f58c1a402068a
Georgios Konstantopoulos <me@gakonst.com>
2019-07-18 10:28:59 +0300

    feat(eth-se) Barebones speculative nonce fetching middleware
    In the future this will be used to combat race conditions in which the engine is trying to sign multiple settlement transactions in parallel

--- crates/interledger-settlement-engines/src/engines/ethereum_ledger/eth_engine.rs
+++ crates/interledger-settlement-engines/src/engines/ethereum_ledger/eth_engine.rs
@@ -87,6 +87,32 @@ pub struct EthereumLedgerSettlementEngine<S, Si, A> {
     confirmations: u8,
     poll_frequency: Duration,
     connector_url: Url,
+    speculative_nonce_middleware: SpeculativeNonceMiddleware,
+}
+
+#[derive(Clone, Debug)]
+struct SpeculativeNonceMiddleware {
+    web3: Web3<Http>,
+    own_address: Address,
+    latest_confirmed_nonce: u64,
+}
+
+impl SpeculativeNonceMiddleware {
+    fn new(web3: Web3<Http>, own_address: Address) -> Self {
+        Self {
+            web3,
+            own_address,
+            latest_confirmed_nonce: 0,
+        }
+    }
+
+    fn next_nonce(&self) -> impl Future<Item = U256, Error = ()> {
+        // TODO: Implement speculative logic. 
+        self.web3
+            .eth()
+            .transaction_count(self.own_address, None)
+            .map_err(|err| error!("Couldn't fetch nonce: {:?}", err))
+    }
 }
 
 impl<S, Si, A> EthereumLedgerSettlementEngine<S, Si, A>
@@ -114,6 +140,8 @@ where
             own_address: signer.address(),
             token_address,
         };
+        let speculative_nonce_middleware =
+            SpeculativeNonceMiddleware::new(web3.clone(), address.own_address);
         let engine = EthereumLedgerSettlementEngine {
             web3,
             store,
@@ -123,6 +151,7 @@ where
             confirmations,
             poll_frequency,
             connector_url,
+            speculative_nonce_middleware,
             account_type: PhantomData,
         };
         if watch_incoming {
@@ -322,12 +351,13 @@ where
         token_address: Option<Address>,
     ) -> Box<dyn Future<Item = H256, Error = ()> + Send> {
         let web3 = self.web3.clone();
+        let nonce_middleware = self.speculative_nonce_middleware.clone();
         let own_address = self.address.own_address;
         let chain_id = self.chain_id;
         let signer = self.signer.clone();
         Box::new(
-            web3.eth()
-                .transaction_count(own_address, None) // 1
+            nonce_middleware
+                .next_nonce()
                 .map_err(move |err| {
                     error!(
                         "Error when querying account {:?} nonce: {:?}",
