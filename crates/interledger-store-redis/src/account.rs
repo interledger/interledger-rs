a1a18bb1a23a422b006bfabb6afbd77d0ef8d51f
Georgios Konstantopoulos <me@gakonst.com>
2019-08-16 12:14:21 +0300

    feat(store-redis) use wrapper AccountId struct that internally uses UUID for indexing accounts

--- crates/interledger-store-redis/src/account.rs
+++ crates/interledger-store-redis/src/account.rs
@@ -4,12 +4,12 @@ use interledger_api::AccountDetails;
 use interledger_btp::BtpAccount;
 use interledger_ccp::{CcpRoutingAccount, RoutingRelation};
 use interledger_http::HttpAccount;
-use interledger_ildcp::IldcpAccount;
 use interledger_packet::Address;
 use interledger_service::Account as AccountTrait;
 use interledger_service_util::{
     MaxPacketAmountAccount, RateLimitAccount, RoundTripTimeAccount, DEFAULT_ROUND_TRIP_TIME,
 };
+use interledger_ildcp::IldcpAccount;
 use interledger_settlement::{SettlementAccount, SettlementEngineDetails};
 use log::error;
 use redis::{from_redis_value, ErrorKind, FromRedisValue, RedisError, ToRedisArgs, Value};
@@ -17,18 +17,20 @@ use redis::{from_redis_value, ErrorKind, FromRedisValue, RedisError, ToRedisArgs
 use ring::aead;
 use serde::Serialize;
 use serde::Serializer;
+use std::fmt::Display;
 use std::{
     collections::HashMap,
     convert::TryFrom,
     str::{self, FromStr},
 };
+use uuid::{parser::ParseError, Uuid};
 
 use url::Url;
 const ACCOUNT_DETAILS_FIELDS: usize = 21;
 
 #[derive(Clone, Debug, Serialize)]
 pub struct Account {
-    pub(crate) id: u64,
+    pub(crate) id: AccountId,
     #[serde(serialize_with = "address_to_string")]
     pub(crate) ilp_address: Address,
     // TODO add additional routes
@@ -99,7 +101,7 @@ where
 }
 
 impl Account {
-    pub fn try_from(id: u64, details: AccountDetails) -> Result<Account, ()> {
+    pub fn try_from(id: AccountId, details: AccountDetails) -> Result<Account, ()> {
         let http_endpoint = if let Some(ref url) = details.http_endpoint {
             Some(Url::parse(url).map_err(|err| error!("Invalid URL: {:?}", err))?)
         } else {
@@ -177,6 +179,56 @@ impl AccountWithEncryptedTokens {
         }
 
         self.account
+    }
+}
+
+// Uuid does not implement ToRedisArgs and FromRedisValue.
+// Rust does not allow implementing foreign traits on foreign data types.
+// As a result, we wrap Uuid in a local data type, and implement the necessary
+// traits for that.
+#[derive(Eq, Hash, Debug, Default, Serialize, Copy, Clone)]
+pub struct AccountId(Uuid);
+
+impl AccountId {
+    pub fn new() -> Self {
+        let uid = Uuid::new_v4();
+        AccountId(uid.clone())
+    }
+}
+
+impl PartialEq for AccountId {
+    fn eq(&self, other: &AccountId) -> bool {
+        self.0.eq(&other.0)
+    }
+}
+
+impl FromStr for AccountId {
+    type Err = ParseError;
+
+    fn from_str(src: &str) -> Result<Self, Self::Err> {
+        let uid = Uuid::from_str(&src)?;
+        Ok(AccountId(uid.clone()))
+    }
+}
+
+impl Display for AccountId {
+    fn fmt(&self, f: &mut ::std::fmt::Formatter) -> Result<(), ::std::fmt::Error> {
+        f.write_str(&self.0.to_hyphenated().to_string())
+    }
+}
+
+impl ToRedisArgs for AccountId {
+    fn write_redis_args(&self, out: &mut Vec<Vec<u8>>) {
+        self.0.to_hyphenated().to_string().write_redis_args(out);
+    }
+}
+
+impl FromRedisValue for AccountId {
+    fn from_redis_value(v: &Value) -> Result<Self, RedisError> {
+        let account_id = String::from_redis_value(v)?;
+        let uid = Uuid::from_str(&account_id)
+            .map_err(|_| RedisError::from((ErrorKind::TypeError, "Invalid account id string")))?;
+        Ok(AccountId(uid))
     }
 }
 
@@ -366,7 +418,7 @@ fn get_bool(key: &str, map: &HashMap<String, Value>) -> bool {
 }
 
 impl AccountTrait for Account {
-    type AccountId = u64;
+    type AccountId = AccountId;
 
     fn id(&self) -> Self::AccountId {
         self.id
@@ -374,6 +426,7 @@ impl AccountTrait for Account {
 }
 
 impl IldcpAccount for Account {
+
     fn client_address(&self) -> &Address {
         &self.ilp_address
     }
@@ -489,8 +542,9 @@ mod redis_account {
 
     #[test]
     fn from_account_details() {
-        let account = Account::try_from(10, ACCOUNT_DETAILS.clone()).unwrap();
-        assert_eq!(account.id(), 10);
+        let id = AccountId::new();
+        let account = Account::try_from(id, ACCOUNT_DETAILS.clone()).unwrap();
+        assert_eq!(account.id(), id);
         assert_eq!(
             account.get_http_auth_token().unwrap(),
             "outgoing_auth_token"
