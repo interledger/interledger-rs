b96a57a263f520ba3de689e982fb0f7addb86feb
Georgios Konstantopoulos <me@gakonst.com>
2019-08-17 16:14:09 +0300

    Revert "remove unused idempotency implementations (#221)"
    This reverts commit 0ebf0d32169759de296f9b088d836e4536db4f78.

--- crates/interledger-settlement-engines/src/stores/mod.rs
+++ crates/interledger-settlement-engines/src/stores/mod.rs
@@ -1,10 +1,33 @@
+use bytes::Bytes;
 use futures::future::Future;
+use http::StatusCode;
 
 pub mod redis_ethereum_ledger;
 pub mod redis_store_common;
 
 #[cfg(test)]
 pub mod test_helpers;
+
+pub type IdempotentEngineData = (StatusCode, Bytes, [u8; 32]);
+
+pub trait IdempotentEngineStore {
+    /// Returns the API response that was saved when the idempotency key was used
+    /// Also returns a hash of the input data which resulted in the response
+    fn load_idempotent_data(
+        &self,
+        idempotency_key: String,
+    ) -> Box<dyn Future<Item = Option<IdempotentEngineData>, Error = ()> + Send>;
+
+    /// Saves the data that was passed along with the api request for later
+    /// The store MUST also save a hash of the input, so that it errors out on requests
+    fn save_idempotent_data(
+        &self,
+        idempotency_key: String,
+        input_hash: [u8; 32],
+        status_code: StatusCode,
+        data: Bytes,
+    ) -> Box<dyn Future<Item = (), Error = ()> + Send>;
+}
 
 pub trait LeftoversStore {
     type AssetType;
--- crates/interledger-settlement-engines/src/stores/redis_store_common.rs
+++ crates/interledger-settlement-engines/src/stores/redis_store_common.rs
@@ -1,7 +1,12 @@
+use crate::stores::{IdempotentEngineData, IdempotentEngineStore};
+use bytes::Bytes;
 use futures::{future::result, Future};
-use redis::{self, r#async::SharedConnection, Client, ConnectionInfo};
+use http::StatusCode;
+use redis::{self, cmd, r#async::SharedConnection, Client, ConnectionInfo, PipelineCommands};
+use std::collections::HashMap as SlowHashMap;
+use std::str::FromStr;
 
-use log::{debug, error};
+use log::{debug, error, trace};
 
 pub struct EngineRedisStoreBuilder {
     redis_uri: ConnectionInfo,
@@ -32,4 +37,123 @@ impl EngineRedisStoreBuilder {
 #[derive(Clone)]
 pub struct EngineRedisStore {
     pub connection: SharedConnection,
+}
+
+impl IdempotentEngineStore for EngineRedisStore {
+    fn load_idempotent_data(
+        &self,
+        idempotency_key: String,
+    ) -> Box<dyn Future<Item = Option<IdempotentEngineData>, Error = ()> + Send> {
+        let idempotency_key_clone = idempotency_key.clone();
+        Box::new(
+            cmd("HGETALL")
+                .arg(idempotency_key.clone())
+                .query_async(self.connection.clone())
+                .map_err(move |err| {
+                    error!(
+                        "Error loading idempotency key {}: {:?}",
+                        idempotency_key_clone, err
+                    )
+                })
+                .and_then(
+                    move |(_connection, ret): (_, SlowHashMap<String, String>)| {
+                        if let (Some(status_code), Some(data), Some(input_hash_slice)) = (
+                            ret.get("status_code"),
+                            ret.get("data"),
+                            ret.get("input_hash"),
+                        ) {
+                            trace!("Loaded idempotency key {:?} - {:?}", idempotency_key, ret);
+                            let mut input_hash: [u8; 32] = Default::default();
+                            input_hash.copy_from_slice(input_hash_slice.as_ref());
+                            Ok(Some((
+                                StatusCode::from_str(status_code).unwrap(),
+                                Bytes::from(data.clone()),
+                                input_hash,
+                            )))
+                        } else {
+                            Ok(None)
+                        }
+                    },
+                ),
+        )
+    }
+
+    fn save_idempotent_data(
+        &self,
+        idempotency_key: String,
+        input_hash: [u8; 32],
+        status_code: StatusCode,
+        data: Bytes,
+    ) -> Box<dyn Future<Item = (), Error = ()> + Send> {
+        let mut pipe = redis::pipe();
+        pipe.atomic()
+            .cmd("HMSET") // cannot use hset_multiple since data and status_code have different types
+            .arg(&idempotency_key)
+            .arg("status_code")
+            .arg(status_code.as_u16())
+            .arg("data")
+            .arg(data.as_ref())
+            .arg("input_hash")
+            .arg(&input_hash)
+            .ignore()
+            .expire(&idempotency_key, 86400)
+            .ignore();
+        Box::new(
+            pipe.query_async(self.connection.clone())
+                .map_err(|err| error!("Error caching: {:?}", err))
+                .and_then(move |(_connection, _): (_, Vec<String>)| {
+                    trace!(
+                        "Cached {:?}: {:?}, {:?}",
+                        idempotency_key,
+                        status_code,
+                        data,
+                    );
+                    Ok(())
+                }),
+        )
+    }
+}
+
+// add tests for idempotency from other store
+#[cfg(test)]
+mod tests {
+    use super::super::test_helpers::store_helpers::{block_on, test_store, IDEMPOTENCY_KEY};
+    use super::*;
+
+    #[test]
+    fn saves_and_loads_idempotency_key_data_properly() {
+        block_on(test_store().and_then(|(store, context)| {
+            let input_hash: [u8; 32] = Default::default();
+            store
+                .save_idempotent_data(
+                    IDEMPOTENCY_KEY.clone(),
+                    input_hash,
+                    StatusCode::OK,
+                    Bytes::from("TEST"),
+                )
+                .map_err(|err| eprintln!("Redis error: {:?}", err))
+                .and_then(move |_| {
+                    store
+                        .load_idempotent_data(IDEMPOTENCY_KEY.clone())
+                        .map_err(|err| eprintln!("Redis error: {:?}", err))
+                        .and_then(move |data1| {
+                            assert_eq!(
+                                data1.unwrap(),
+                                (StatusCode::OK, Bytes::from("TEST"), input_hash)
+                            );
+                            let _ = context;
+
+                            store
+                                .load_idempotent_data("asdf".to_string())
+                                .map_err(|err| eprintln!("Redis error: {:?}", err))
+                                .and_then(move |data2| {
+                                    assert!(data2.is_none());
+                                    let _ = context;
+                                    Ok(())
+                                })
+                        })
+                })
+        }))
+        .unwrap()
+    }
 }
