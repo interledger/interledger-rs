23c012620f40ffb8e05d579709530c4952278be4
Georgios Konstantopoulos <me@gakonst.com>
2019-08-22 15:15:22 +0300

    feat(service): Implement username sanitization

--- crates/interledger-service/src/auth/username.rs
+++ crates/interledger-service/src/auth/username.rs
@@ -2,20 +2,21 @@
 /// 1. Normalizes string input using NFKC unicode normalization (using this function)
 /// 2. Validates that the string matches the regex /^\w{1,32}$/u (the unicode flag is set)
 /// 3. Uses case-folding to convert to lowercase in a language-sensitive way (possibly using unicase)
-/// 4. (Possibly) Check that all characters are part of the same script or disallow "confusable" characters (using unicode-script or unicode_skeleton)
-///
-///
 use regex::Regex;
 use std::fmt::Display;
 use std::str::FromStr;
 
 use lazy_static::lazy_static;
 use serde::{Deserialize, Serialize};
+use unicase::UniCase;
+use unicode_normalization::UnicodeNormalization;
 
 lazy_static! {
-    static ref USERNAME_PATTERN: Regex = Regex::new(r"/^\w{1,32}$/u").unwrap();
+    // No need to add unicode flags, already supported by the crate.
+    static ref USERNAME_PATTERN: Regex = Regex::new(r"^\w{2,32}$").unwrap();
 }
 
+/// Usernames can be unicode and must be between 2 and 32 characters
 #[derive(Clone, Debug, Serialize, Deserialize, Eq, PartialEq)]
 pub struct Username(String);
 
@@ -44,7 +45,12 @@ impl FromStr for Username {
     type Err = ();
 
     fn from_str(src: &str) -> Result<Self, Self::Err> {
-        Ok(Username(src.to_owned()))
+        let unicode_normalized = src.nfkc().collect::<String>();
+        if !USERNAME_PATTERN.is_match(&unicode_normalized) {
+            return Err(());
+        }
+        let casefolded = UniCase::new(unicode_normalized);
+        Ok(Username(casefolded.to_string()))
     }
 }
 
@@ -53,7 +59,38 @@ mod tests {
     use super::*;
 
     #[test]
-    fn parses_correctly() {
+    fn too_long_name() {
+        assert!(Username::from_str("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa").is_err());
+    }
+
+    #[test]
+    fn too_short_name() {
+        assert!(Username::from_str("a").is_err());
+    }
+
+    #[test]
+    fn mixed_case() {
+        assert_eq!(
+            Username::from_str("A_lic123").unwrap().to_string(),
+            "A_lic123"
+        );
+    }
+
+    #[test]
+    fn numbers_symbols_work() {
+        assert_eq!(
+            Username::from_str("a_lic123").unwrap().to_string(),
+            "a_lic123"
+        );
+    }
+
+    #[test]
+    fn works_with_unicode() {
+        assert_eq!(Username::from_str("山本").unwrap().to_string(), "山本");
+    }
+
+    #[test]
+    fn works() {
         assert_eq!(
             Username::from_str("alice").unwrap(),
             Username("alice".to_owned())
