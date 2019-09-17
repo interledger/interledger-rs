e010ddea6a06971a8375807e763e75adc728f684
Taiga Nakayama <dora@dora-gt.jp>
2019-09-18 04:45:46 +0900

    ci(circleci): enables run-md-test.sh on CircleCI
    Signed-off-by: Taiga Nakayama <dora@dora-gt.jp>

--- docker/node.dockerfile
+++ docker/node.dockerfile
@@ -1,18 +1,24 @@
 # Build Interledger node into standalone binary
 FROM clux/muslrust:stable as rust
+ARG CARGO_BUILD_OPTION=""
+ARG RUST_BIN_DIR_NAME="debug"
+
+RUN echo "Building profile: ${CARGO_BUILD_OPTION}, output dir: ${RUST_BIN_DIR_NAME}"
 
 WORKDIR /usr/src
 COPY ./Cargo.toml /usr/src/Cargo.toml
+COPY ./Cargo.lock /usr/src/Cargo.lock
 COPY ./crates /usr/src/crates
 
-RUN cargo build --release --package ilp-node
-# RUN cargo build --package ilp-node
+RUN cargo build ${CARGO_BUILD_OPTION} --package ilp-node --bin ilp-node
 
 # Deploy compiled binary to another container
 FROM alpine
+ARG CARGO_BUILD_OPTION=""
+ARG RUST_BIN_DIR_NAME="debug"
 
 # Expose ports for HTTP and BTP
-# - 7770: HTTP - ILP over HTTP, API
+# - 7770: HTTP - ILP over HTTP, API, BTP
 # - 7771: HTTP - settlement
 EXPOSE 7770
 EXPOSE 7771
@@ -22,11 +28,8 @@ RUN apk --no-cache add ca-certificates
 
 # Copy Interledger binary
 COPY --from=rust \
-    /usr/src/target/x86_64-unknown-linux-musl/release/ilp-node \
+    /usr/src/target/x86_64-unknown-linux-musl/${RUST_BIN_DIR_NAME}/ilp-node \
     /usr/local/bin/ilp-node
-# COPY --from=rust \
-#     /usr/src/target/x86_64-unknown-linux-musl/debug/ilp-node \
-#     /usr/local/bin/ilp-node
 
 WORKDIR /opt/app
 
--- scripts/run-md-test.sh
+++ scripts/run-md-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-function init_test() {
+function clear_environment() {
     clear_logs
     clear_docker
     free_ports
@@ -12,9 +12,9 @@ function clear_logs() {
 }
 
 function clear_docker() {
-    docker network rm interledger 2>/dev/null
     docker stop $(docker ps -aq) 2>/dev/null
     docker rm $(docker ps -aq) 2>/dev/null
+    docker network rm interledger 2>/dev/null
 }
 
 function free_ports() {
@@ -22,6 +22,13 @@ function free_ports() {
     for port in $(seq 6379 6385); do
         if lsof -Pi :${port} -sTCP:LISTEN -t >/dev/null ; then
             redis-cli -p ${port} shutdown
+        fi
+    done
+
+    # ports of redis
+    for port in $(seq 6379 6385); do
+        if lsof -tPi :${port} >/dev/null ; then
+            kill `lsof -tPi :${port}`
         fi
     done
 
@@ -42,54 +49,88 @@ function clear_redis_file() {
 
 # $1 = target directory
 # $2 = docker mode in [01]
+# $3 = test name filter
 #
 # test_example examples/eth-settlement 0
 function test_example() {
     local target_directory=$1
     local docker_mode=$2
+    local test_name_filter=$3
     local target_name=$(basename $target_directory)
+    local result
 
-    cd $target_directory
+    pushd $target_directory > /dev/null
     if [ $docker_mode -eq 1 ]; then
-        printf "\e[33;1m%b [%d/%d]\e[m\n" "Testing \"${target_name}\" on docker mode." "$((TESTING_INDEX + 1))" "${TESTS_TOTAL}"
-        init_test; USE_DOCKER=1 $RUN_MD .
-        return $?
+        printf "\e[33;1mTesting \"${target_name}\" on docker mode. [%d/%d]\e[m\n" "$((TESTING_INDEX + 1))" "${TESTS_TOTAL}"
+        clear_environment; TEST_MODE=1 USE_DOCKER=1 $RUN_MD .
+        result=$?
     else
-        printf "\e[33;1m%b [%d/%d]\e[m\n" "Testing \"${target_name}\" on non-docker mode." "$((TESTING_INDEX + 1))" "${TESTS_TOTAL}"
-        init_test; $RUN_MD .
-        return $?
+        printf "\e[33;1mTesting \"${target_name}\" on non-docker mode. [%d/%d]\e[m\n" "$((TESTING_INDEX + 1))" "${TESTS_TOTAL}"
+        clear_environment; TEST_MODE=1 $RUN_MD .
+        result=$?
     fi
+    mkdir -p ${LOG_DIR}/${target_name}
+    mv logs ${LOG_DIR}/${target_name}/${docker_mode}
+    # there may not be logs when testing non-settlement ones
+    mv ${SETTLEMENT_ENGINE_DIR}/logs/* ${LOG_DIR}/${target_name}/${docker_mode}/ &>/dev/null
+    popd > /dev/null
+    return $result
 }
 
 # Adds all directories in `examples` directory as test targets.
 function add_example_tests() {
-    # This will add tests like "test_example eth-settlement 0" which means an example test of
+    local test_name_filter=$1
+    local docker_mode_filter=$2
+
+    # This will add tests like "test_example examples/eth-settlement 0" which means an example test of
     # eth-settlement in non-docker mode.
-    for directory in $(find $BASE_DIR/../examples/* -type d -maxdepth 0); do
-        TESTS+=("test_example ${directory} 0") # this cannot contain space in the directory path
-        TESTS+=("test_example ${directory} 1")
+    for directory in $(find $BASE_DIR/../examples/* -maxdepth 0 -type d); do
+        if [ -n "${test_name_filter}" ] && [[ ! "${directory}" =~ ${test_name_filter} ]]; then
+            printf "\e[33;1mSkipping \"%s\"\e[m\n" "${directory}"
+            continue
+        fi
+        if [ -n "${docker_mode_filter}" ]; then
+            printf "\e[33;1mAdding only USE_DOCKER=%d for \"%s\"\e[m\n" "${docker_mode_filter}" "${directory}"
+            TESTS+=("test_example ${directory} ${docker_mode_filter} ${test_name_filter}")
+        else
+            TESTS+=("test_example ${directory} 0 ${test_name_filter}") # this cannot contain space in the directory path
+            TESTS+=("test_example ${directory} 1 ${test_name_filter}")
+        fi
     done
 }
 
+# $1 = test name filter
+# $2 = docker mode filter
+
 BASE_DIR=$(cd $(dirname $0); pwd)
 RUN_MD=$BASE_DIR/run-md.sh
+LOG_DIR=/tmp/run-md-test/logs
+export SETTLEMENT_ENGINE_INSTALLL_DIR=$(cd ~; pwd)
+SETTLEMENT_ENGINE_DIR=${SETTLEMENT_ENGINE_INSTALLL_DIR}/settlement-engines
 
+# set up example tests
 TESTS=()
-add_example_tests
+add_example_tests $@
+
+# set up log dir for CircleCI
+# the log files will be put as artifacts
+rm -rf $LOG_DIR
+mkdir -p $LOG_DIR
+
 TESTS_TOTAL=${#TESTS[@]}
 TESTS_FAILED=0
 TESTING_INDEX=0
 for test in "${TESTS[@]}"; do
-    TEST_MODE=1 $test
+    $test
     if [ $? -ne 0 ]; then
         TESTS_FAILED=$((TESTS_FAILED + 1))
     fi
     TESTING_INDEX=$((TESTING_INDEX + 1))
 done
 if [ $TESTS_FAILED -eq 0 ]; then
-    printf "\e[32;1m%b [%d/%d]\e[m\n" "All tests passed!" "${TESTS_TOTAL}" "${TESTS_TOTAL}"
+    printf "\e[32;1mAll tests passed! [%d/%d]\e[m\n" $((TESTS_TOTAL - TESTS_FAILED)) "${TESTS_TOTAL}"
     exit 0
 else
-    printf "\e[31;1m%b [%d/%d]\e[m\n" "Some tests failed." "${TESTS_FAILED}" "${TESTS_TOTAL}"
+    printf "\e[31;1mSome tests failed. [%d/%d]\e[m\n" $((TESTS_TOTAL - TESTS_FAILED)) "${TESTS_TOTAL}"
     exit 1
 fi
