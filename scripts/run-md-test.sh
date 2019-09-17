1626405ce1c26399b6e8d91e65985f32fcb9672e
Taiga Nakayama <dora@dora-gt.jp>
2019-09-18 04:45:46 +0900

    ci(circleci): enables run-md-test.sh on CircleCI
    Signed-off-by: Taiga Nakayama <dora@dora-gt.jp>

--- scripts/run-md-test.sh
+++ scripts/run-md-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-function init_test() {
+function clear_environment() {
     clear_logs
     clear_docker
     free_ports
@@ -12,9 +12,9 @@ function clear_logs() {
 }
 
 function clear_docker() {
-    docker network rm interledger 2>/dev/null
     docker stop $(docker ps -aq) 2>/dev/null
     docker rm $(docker ps -aq) 2>/dev/null
+    docker network rm interledger 2>/dev/null
 }
 
 function free_ports() {
@@ -22,6 +22,13 @@ function free_ports() {
     for port in $(seq 6379 6385); do
         if lsof -Pi :${port} -sTCP:LISTEN -t >/dev/null ; then
             redis-cli -p ${port} shutdown
+        fi
+    done
+
+    # ports of redis
+    for port in $(seq 6379 6385); do
+        if lsof -tPi :${port} >/dev/null ; then
+            kill `lsof -tPi :${port}`
         fi
     done
 
@@ -48,24 +55,29 @@ function test_example() {
     local target_directory=$1
     local docker_mode=$2
     local target_name=$(basename $target_directory)
+    local result
 
-    cd $target_directory
+    pushd $target_directory > /dev/null
     if [ $docker_mode -eq 1 ]; then
-        printf "\e[33;1m%b [%d/%d]\e[m\n" "Testing \"${target_name}\" on docker mode." "$((TESTING_INDEX + 1))" "${TESTS_TOTAL}"
-        init_test; USE_DOCKER=1 $RUN_MD .
-        return $?
+        printf "\e[33;1mTesting \"${target_name}\" on docker mode. [%d/%d]\e[m\n" "$((TESTING_INDEX + 1))" "${TESTS_TOTAL}"
+        clear_environment; TEST_MODE=1 USE_DOCKER=1 $RUN_MD .
+        result=$?
     else
-        printf "\e[33;1m%b [%d/%d]\e[m\n" "Testing \"${target_name}\" on non-docker mode." "$((TESTING_INDEX + 1))" "${TESTS_TOTAL}"
-        init_test; $RUN_MD .
-        return $?
+        printf "\e[33;1mTesting \"${target_name}\" on non-docker mode. [%d/%d]\e[m\n" "$((TESTING_INDEX + 1))" "${TESTS_TOTAL}"
+        clear_environment; TEST_MODE=1 $RUN_MD .
+        result=$?
     fi
+    mkdir -p ${LOG_DIR}/${target_name}
+    mv logs ${LOG_DIR}/${target_name}/${docker_mode}
+    popd > /dev/null
+    return $result
 }
 
 # Adds all directories in `examples` directory as test targets.
 function add_example_tests() {
-    # This will add tests like "test_example eth-settlement 0" which means an example test of
+    # This will add tests like "test_example examples/eth-settlement 0" which means an example test of
     # eth-settlement in non-docker mode.
-    for directory in $(find $BASE_DIR/../examples/* -type d -maxdepth 0); do
+    for directory in $(find $BASE_DIR/../examples/* -maxdepth 0 -type d); do
         TESTS+=("test_example ${directory} 0") # this cannot contain space in the directory path
         TESTS+=("test_example ${directory} 1")
     done
@@ -73,23 +85,31 @@ function add_example_tests() {
 
 BASE_DIR=$(cd $(dirname $0); pwd)
 RUN_MD=$BASE_DIR/run-md.sh
+LOG_DIR=/tmp/run-md-test/logs
 
+# set up example tests
 TESTS=()
 add_example_tests
+
+# set up log dir for CircleCI
+# the log files will be put as artifacts
+rm -rf $LOG_DIR
+mkdir -p $LOG_DIR
+
 TESTS_TOTAL=${#TESTS[@]}
 TESTS_FAILED=0
 TESTING_INDEX=0
 for test in "${TESTS[@]}"; do
-    TEST_MODE=1 $test
+    $test
     if [ $? -ne 0 ]; then
         TESTS_FAILED=$((TESTS_FAILED + 1))
     fi
     TESTING_INDEX=$((TESTING_INDEX + 1))
 done
 if [ $TESTS_FAILED -eq 0 ]; then
-    printf "\e[32;1m%b [%d/%d]\e[m\n" "All tests passed!" "${TESTS_TOTAL}" "${TESTS_TOTAL}"
+    printf "\e[32;1mAll tests passed! [%d/%d]\e[m\n" $((TESTS_TOTAL - TESTS_FAILED)) "${TESTS_TOTAL}"
     exit 0
 else
-    printf "\e[31;1m%b [%d/%d]\e[m\n" "Some tests failed." "${TESTS_FAILED}" "${TESTS_TOTAL}"
+    printf "\e[31;1mSome tests failed. [%d/%d]\e[m\n" $((TESTS_TOTAL - TESTS_FAILED)) "${TESTS_TOTAL}"
     exit 1
 fi
