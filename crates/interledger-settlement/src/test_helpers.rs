b66e5ba34db78cb08d9f3375a63e15a354a2d936
Georgios Konstantopoulos <me@gakonst.com>
2019-09-27 10:25:40 +0900

    test(settlement): add failing test for connector processing leftover values on incoming settlement

--- crates/interledger-settlement/src/test_helpers.rs
+++ crates/interledger-settlement/src/test_helpers.rs
@@ -1,5 +1,7 @@
 use super::*;
-use crate::SettlementEngineDetails;
+use crate::api::scale_with_precision_loss;
+use crate::Convert;
+use crate::{LeftoversStore, SettlementEngineDetails};
 use futures::{
     future::{err, ok},
     Future,
@@ -27,6 +29,7 @@ pub struct TestAccount {
     pub url: Url,
     pub ilp_address: Address,
     pub no_details: bool,
+    pub balance: i64,
 }
 
 lazy_static! {
@@ -71,10 +74,11 @@ impl SettlementAccount for TestAccount {
 // Test Store
 #[derive(Clone)]
 pub struct TestStore {
-    pub accounts: Arc<Vec<TestAccount>>,
+    pub accounts: Arc<RwLock<Vec<TestAccount>>>,
     pub should_fail: bool,
     pub cache: Arc<RwLock<HashMap<String, IdempotentData>>>,
     pub cache_hits: Arc<RwLock<u64>>,
+    pub uncredited_settlement_amount: Arc<RwLock<HashMap<u64, (BigUint, u8)>>>,
 }
 
 impl SettlementStore for TestStore {
@@ -82,10 +86,16 @@ impl SettlementStore for TestStore {
 
     fn update_balance_for_incoming_settlement(
         &self,
-        _account_id: <Self::Account as Account>::AccountId,
-        _amount: u64,
+        account_id: <Self::Account as Account>::AccountId,
+        amount: u64,
         _idempotency_key: Option<String>,
     ) -> Box<dyn Future<Item = (), Error = ()> + Send> {
+        let mut accounts = self.accounts.write();
+        for mut a in &mut *accounts {
+            if a.id() == account_id {
+                a.balance += amount as i64;
+            }
+        }
         let ret = if self.should_fail { err(()) } else { ok(()) };
         Box::new(ret)
     }
@@ -137,6 +147,7 @@ impl AccountStore for TestStore {
     ) -> Box<dyn Future<Item = Vec<Self::Account>, Error = ()> + Send> {
         let accounts: Vec<TestAccount> = self
             .accounts
+            .read()
             .iter()
             .filter_map(|account| {
                 if account_ids.contains(&account.id) {
@@ -162,14 +173,105 @@ impl AccountStore for TestStore {
     }
 }
 
+impl LeftoversStore for TestStore {
+    type AccountId = u64;
+    type AssetType = BigUint;
+
+    fn save_uncredited_settlement_amount(
+        &self,
+        account_id: Self::AccountId,
+        uncredited_settlement_amount: (Self::AssetType, u8),
+    ) -> Box<dyn Future<Item = (), Error = ()> + Send> {
+        let mut guard = self.uncredited_settlement_amount.write();
+        if let Some(leftovers) = (*guard).get_mut(&account_id) {
+            if leftovers.1 > uncredited_settlement_amount.1 {
+                // the current leftovers maintain the scale so we just need to
+                // upscale the provided leftovers to the existing leftovers' scale
+                let scaled = uncredited_settlement_amount
+                    .0
+                    .normalize_scale(ConvertDetails {
+                        from: uncredited_settlement_amount.1,
+                        to: leftovers.1,
+                    })
+                    .unwrap();
+                *leftovers = (leftovers.0.clone() + scaled, leftovers.1);
+            } else if leftovers.1 == uncredited_settlement_amount.1 {
+                *leftovers = (
+                    leftovers.0.clone() + uncredited_settlement_amount.0,
+                    leftovers.1,
+                );
+            } else {
+                // if the scale of the provided leftovers is bigger than
+                // existing scale then we update the scale of the leftovers'
+                // scale
+                let scaled = leftovers
+                    .0
+                    .normalize_scale(ConvertDetails {
+                        from: leftovers.1,
+                        to: uncredited_settlement_amount.1,
+                    })
+                    .unwrap();
+                *leftovers = (
+                    uncredited_settlement_amount.0 + scaled,
+                    uncredited_settlement_amount.1,
+                );
+            }
+        } else {
+            (*guard).insert(account_id, uncredited_settlement_amount);
+        }
+        Box::new(ok(()))
+    }
+
+    fn load_uncredited_settlement_amount(
+        &self,
+        account_id: Self::AccountId,
+        local_scale: u8,
+    ) -> Box<dyn Future<Item = Self::AssetType, Error = ()> + Send> {
+        let mut guard = self.uncredited_settlement_amount.write();
+        if let Some(l) = guard.get_mut(&account_id) {
+            let ret = l.clone();
+            let (scaled_leftover_amount, leftover_precision_loss) =
+                scale_with_precision_loss(ret.0, local_scale, ret.1);
+            // save the new leftovers
+            *l = (leftover_precision_loss, std::cmp::max(local_scale, ret.1));
+            Box::new(ok(scaled_leftover_amount))
+        } else {
+            Box::new(ok(BigUint::from(0u32)))
+        }
+    }
+
+    fn get_uncredited_settlement_amount(
+        &self,
+        account_id: u64,
+    ) -> Box<dyn Future<Item = (Self::AssetType, u8), Error = ()> + Send> {
+        let leftovers = self.uncredited_settlement_amount.read();
+        Box::new(ok(if let Some(a) = leftovers.get(&account_id) {
+            a.clone()
+        } else {
+            (BigUint::from(0u32), 1)
+        }))
+    }
+}
+
 impl TestStore {
     pub fn new(accs: Vec<TestAccount>, should_fail: bool) -> Self {
         TestStore {
-            accounts: Arc::new(accs),
+            accounts: Arc::new(RwLock::new(accs)),
             should_fail,
             cache: Arc::new(RwLock::new(HashMap::new())),
             cache_hits: Arc::new(RwLock::new(0)),
+            uncredited_settlement_amount: Arc::new(RwLock::new(HashMap::new())),
         }
+    }
+
+    pub fn get_balance(&self, account_id: u64) -> i64 {
+        let accounts = &*self.accounts.read();
+        for a in accounts {
+            if a.id() == account_id {
+                return a.balance;
+            }
+        }
+        0
     }
 }
 
@@ -182,6 +284,7 @@ impl TestAccount {
             url: Url::parse(url).unwrap(),
             ilp_address: Address::from_str(ilp_address).unwrap(),
             no_details: false,
+            balance: 0,
         }
     }
 }
