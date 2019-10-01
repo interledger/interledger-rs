c35f88d6ea334ffb7bd7fce3376ec4f4078a1abe
Ben Striegel <bstrie@workbook>
2019-10-01 06:27:13 -0400

    feat(ilp-cli): addressing review comments and adding missing commands

--- crates/ilp-cli/src/interpreter.rs
+++ crates/ilp-cli/src/interpreter.rs
@@ -1,17 +1,17 @@
 #![allow(unused_variables)]
 
-use clap::{App, ArgMatches};
-use reqwest::{Client, Response};
-use std::process::exit;
+use clap::ArgMatches;
+use reqwest::{self, Client, Response};
+use std::collections::HashMap;
 
-pub fn run<'a, 'b>(mut app: App<'a, 'b>) {
-    // Parse the CLI input using the defined arguments
-    let matches = app.clone().get_matches();
+pub enum Error {
+    UsageErr(&'static str),
+    ClientErr(reqwest::Error),
+}
 
-    let node = Node {
+pub fn run<'a, 'b>(matches: &ArgMatches) -> Result<Response, Error> {
+    let client = NodeClient {
         client: Client::new(),
-        // `--auth` is a required argument, so will never be None
-        auth: matches.value_of("authorization_key").unwrap(),
         // `--node` has a a default value, so will never be None
         url: matches.value_of("node_url").unwrap(),
     };
@@ -21,203 +21,252 @@ pub fn run<'a, 'b>(mut app: App<'a, 'b>) {
         // Execute the specified subcommand
         (ilp_cli_subcommand, Some(ilp_cli_matches)) => {
             // Send HTTP request
-            let response = match ilp_cli_subcommand {
+            match ilp_cli_subcommand {
                 "accounts" => match ilp_cli_matches.subcommand() {
                     (accounts_subcommand, Some(accounts_matches)) => match accounts_subcommand {
-                        "balance" => node.get_account_balance(accounts_matches),
-                        "create" => node.post_accounts(accounts_matches),
-                        "delete" => node.delete_account(accounts_matches),
-                        "incoming-payments" => node.ws_account_payments_incoming(accounts_matches),
-                        "info" => node.get_account(accounts_matches),
-                        "list" => node.get_accounts(accounts_matches),
+                        "balance" => client.get_account_balance(accounts_matches),
+                        "create" => client.post_accounts(accounts_matches),
+                        "delete" => client.delete_account(accounts_matches),
+                        "incoming-payments" => {
+                            client.ws_account_payments_incoming(accounts_matches)
+                        }
+                        "info" => client.get_account(accounts_matches),
+                        "list" => client.get_accounts(accounts_matches),
                         "update" => {
                             if accounts_matches.is_present("is_admin") {
-                                node.put_account(accounts_matches)
+                                client.put_account(accounts_matches)
                             } else {
-                                node.put_account_settings(accounts_matches)
+                                client.put_account_settings(accounts_matches)
                             }
                         }
                         command => panic!("Unhandled `ilp-cli accounts` subcommand: {}", command),
                     },
-                    // TODO: subcommand-specific help output
-                    _ => {
-                        app.print_help().unwrap();
-                        exit(1);
-                    }
+                    _ => Err(Error::UsageErr("ilp-cli help accounts")),
                 },
-                "pay" => node.post_account_payments(ilp_cli_matches),
+                "pay" => client.post_account_payments(ilp_cli_matches),
                 "rates" => match ilp_cli_matches.subcommand() {
                     (rates_subcommand, Some(rates_matches)) => match rates_subcommand {
-                        "list" => node.get_rates(rates_matches),
-                        "set-all" => node.put_rates(rates_matches),
+                        "list" => client.get_rates(rates_matches),
+                        "set-all" => client.put_rates(rates_matches),
                         command => panic!("Unhandled `ilp-cli rates` subcommand: {}", command),
                     },
-                    _ => {
-                        app.print_help().unwrap();
-                        exit(1);
-                    }
+                    _ => Err(Error::UsageErr("ilp-cli help rates")),
                 },
                 "routes" => match ilp_cli_matches.subcommand() {
                     (routes_subcommand, Some(routes_matches)) => match routes_subcommand {
-                        "list" => node.get_routes(routes_matches),
-                        "set" => node.put_route_static(routes_matches),
-                        "set-all" => node.put_routes_static(routes_matches),
+                        "list" => client.get_routes(routes_matches),
+                        "set" => client.put_route_static(routes_matches),
+                        "set-all" => client.put_routes_static(routes_matches),
                         command => panic!("Unhandled `ilp-cli routes` subcommand: {}", command),
                     },
-                    _ => {
-                        app.print_help().unwrap();
-                        exit(1);
-                    }
+                    _ => Err(Error::UsageErr("ilp-cli help routes")),
                 },
                 "settlement-engines" => match ilp_cli_matches.subcommand() {
                     (settlement_engines_subcommand, Some(settlement_engines_matches)) => {
                         match settlement_engines_subcommand {
-                            "set-all" => node.put_settlement_engines(settlement_engines_matches),
+                            "set-all" => client.put_settlement_engines(ilp_cli_matches),
                             command => panic!(
                                 "Unhandled `ilp-cli settlement-engines` subcommand: {}",
                                 command
                             ),
                         }
                     }
-                    _ => {
-                        app.print_help().unwrap();
-                        exit(1);
-                    }
+                    _ => Err(Error::UsageErr("ilp-cli help settlement-engines")),
                 },
-                "status" => node.get_root(ilp_cli_matches),
+                "status" => client.get_root(ilp_cli_matches),
                 command => panic!("Unhandled `ilp-cli` subcommand: {}", command),
-            };
-
-            // Handle HTTP response
-            match response {
-                Err(e) => {
-                    eprintln!("ILP CLI error: failed to send request: {}", e);
-                    exit(1);
-                }
-                Ok(mut res) => match res.text() {
-                    Err(e) => {
-                        eprintln!("ILP CLI error: failed to parse response: {}", e);
-                        exit(1);
-                    }
-                    // Final output
-                    Ok(val) => {
-                        if res.status().is_success() {
-                            if !matches.is_present("quiet") {
-                                println!("{}", val)
-                            }
-                        } else {
-                            eprintln!(
-                                "ILP CLI error: unsuccessful response from node: {}: {}",
-                                res.status(),
-                                val
-                            );
-                            exit(1);
-                        }
-                    }
-                },
             }
         }
-        // No subcommand identified within parsed input
-        _ => {
-            app.print_help().unwrap();
-            exit(1);
-        }
+        _ => Err(Error::UsageErr("ilp-cli help")),
     }
 }
 
-struct Node<'a> {
+struct NodeClient<'a> {
     client: Client,
-    auth: &'a str,
     url: &'a str,
 }
 
-impl Node<'_> {
-    fn get_account_balance(&self, matches: &ArgMatches) -> reqwest::Result<Response> {
-        let user = matches.value_of("account_username").unwrap();
+impl NodeClient<'_> {
+    fn get_account_balance(&self, matches: &ArgMatches) -> Result<Response, Error> {
+        let (auth, mut args) = extract_args(matches);
+        let user = args.remove("account_username").unwrap();
         self.client
             .get(&format!("{}/accounts/{}/balance", self.url, user))
-            .bearer_auth(self.auth)
+            .bearer_auth(auth)
             .send()
+            .map_err(Error::ClientErr)
     }
 
-    fn post_accounts(&self, matches: &ArgMatches) -> reqwest::Result<Response> {
-        let args = extract_args(matches);
+    fn post_accounts(&self, matches: &ArgMatches) -> Result<Response, Error> {
+        let (auth, args) = extract_args(matches);
         self.client
             .post(&format!("{}/accounts", self.url))
-            .bearer_auth(self.auth)
+            .bearer_auth(auth)
             .json(&args)
             .send()
+            .map_err(Error::ClientErr)
     }
 
-    fn delete_account(&self, matches: &ArgMatches) -> reqwest::Result<Response> {
+    fn delete_account(&self, matches: &ArgMatches) -> Result<Response, Error> {
+        let (auth, args) = extract_args(matches);
+        self.client
+            .delete(&format!("{}/accounts/{}", self.url, args["username"]))
+            .bearer_auth(auth)
+            .send()
+            .map_err(Error::ClientErr)
+    }
+
+    fn ws_account_payments_incoming(&self, matches: &ArgMatches) -> Result<Response, Error> {
         unimplemented!()
     }
 
-    fn ws_account_payments_incoming(&self, matches: &ArgMatches) -> reqwest::Result<Response> {
-        unimplemented!()
+    fn get_account(&self, matches: &ArgMatches) -> Result<Response, Error> {
+        let (auth, args) = extract_args(matches);
+        self.client
+            .get(&format!("{}/accounts/{}", self.url, args["username"]))
+            .bearer_auth(auth)
+            .send()
+            .map_err(Error::ClientErr)
     }
 
-    fn get_account(&self, matches: &ArgMatches) -> reqwest::Result<Response> {
-        unimplemented!()
+    fn get_accounts(&self, matches: &ArgMatches) -> Result<Response, Error> {
+        let (auth, _) = extract_args(matches);
+        self.client
+            .get(&format!("{}/accounts", self.url))
+            .bearer_auth(auth)
+            .send()
+            .map_err(Error::ClientErr)
     }
 
-    fn get_accounts(&self, matches: &ArgMatches) -> reqwest::Result<Response> {
-        unimplemented!()
+    fn put_account(&self, matches: &ArgMatches) -> Result<Response, Error> {
+        let (auth, mut args) = extract_args(matches);
+        let user = args.remove("username").unwrap();
+        self.client
+            .put(&format!("{}/accounts/{}", self.url, user))
+            .bearer_auth(auth)
+            .json(&args)
+            .send()
+            .map_err(Error::ClientErr)
     }
 
-    fn put_account(&self, matches: &ArgMatches) -> reqwest::Result<Response> {
-        unimplemented!()
+    fn put_account_settings(&self, matches: &ArgMatches) -> Result<Response, Error> {
+        let (auth, mut args) = extract_args(matches);
+        let user = args.remove("username").unwrap();
+        self.client
+            .put(&format!("{}/accounts/{}/settings", self.url, user))
+            .bearer_auth(auth)
+            .json(&args)
+            .send()
+            .map_err(Error::ClientErr)
     }
 
-    fn put_account_settings(&self, matches: &ArgMatches) -> reqwest::Result<Response> {
-        unimplemented!()
-    }
-
-    fn post_account_payments(&self, matches: &ArgMatches) -> reqwest::Result<Response> {
-        let mut args = extract_args(matches);
+    fn post_account_payments(&self, matches: &ArgMatches) -> Result<Response, Error> {
+        let (auth, mut args) = extract_args(matches);
         let user = args.remove("sender_username").unwrap();
         self.client
             .post(&format!("{}/accounts/{}/payments", self.url, user))
-            .bearer_auth(&format!("{}:{}", user, self.auth))
+            .bearer_auth(&format!("{}:{}", user, auth))
             .json(&args)
             .send()
+            .map_err(Error::ClientErr)
     }
 
-    fn get_rates(&self, matches: &ArgMatches) -> reqwest::Result<Response> {
+    fn get_rates(&self, matches: &ArgMatches) -> Result<Response, Error> {
+        self.client
+            .get(&format!("{}/rates", self.url))
+            .send()
+            .map_err(Error::ClientErr)
+    }
+
+    fn put_rates(&self, matches: &ArgMatches) -> Result<Response, Error> {
+        let auth = matches.value_of("authorization_key").unwrap();
+        let mut rates = HashMap::new();
+        if let Some(rate_matches) = matches.values_of("rate") {
+            let rate_matches: Vec<&str> = rate_matches.collect();
+            let codes: Vec<String> = rate_matches
+                .iter()
+                .enumerate()
+                .filter(|(i, _)| i % 2 == 0)
+                .map(|(_, code)| code.to_string())
+                .collect();
+            let values: Vec<f64> = rate_matches
+                .iter()
+                .enumerate()
+                .filter(|(i, _)| i % 2 == 1)
+                .map(|(_, value)| value.parse().unwrap())
+                .collect();
+            for (code, value) in codes.into_iter().zip(values.into_iter()) {
+                rates.insert(code, value);
+            }
+        }
+        self.client
+            .put(&format!("{}/rates", self.url))
+            .bearer_auth(auth)
+            .json(&rates)
+            .send()
+            .map_err(Error::ClientErr)
+    }
+
+    fn get_routes(&self, matches: &ArgMatches) -> Result<Response, Error> {
+        self.client
+            .get(&format!("{}/routes", self.url))
+            .send()
+            .map_err(Error::ClientErr)
+    }
+
+    fn put_route_static(&self, matches: &ArgMatches) -> Result<Response, Error> {
         unimplemented!()
     }
 
-    fn put_rates(&self, matches: &ArgMatches) -> reqwest::Result<Response> {
+    fn put_routes_static(&self, matches: &ArgMatches) -> Result<Response, Error> {
+        let auth = matches.value_of("authorization_key").unwrap();
+        let mut combined_routes = HashMap::new();
+        if let Some(route_matches) = matches.values_of("route") {
+            let route_matches: Vec<&str> = route_matches.collect();
+            let prefixes: Vec<String> = route_matches
+                .iter()
+                .enumerate()
+                .filter(|(i, _)| i % 2 == 0)
+                .map(|(_, prefix)| prefix.to_string())
+                .collect();
+            let routes: Vec<String> = route_matches
+                .iter()
+                .enumerate()
+                .filter(|(i, _)| i % 2 == 1)
+                .map(|(_, route)| route.to_string())
+                .collect();
+            for (prefix, route) in prefixes.into_iter().zip(routes.into_iter()) {
+                combined_routes.insert(prefix, route);
+            }
+        }
+        self.client
+            .put(&format!("{}/routes/static", self.url))
+            .bearer_auth(auth)
+            .json(&combined_routes)
+            .send()
+            .map_err(Error::ClientErr)
+    }
+
+    fn put_settlement_engines(&self, matches: &ArgMatches) -> Result<Response, Error> {
         unimplemented!()
     }
 
-    fn get_routes(&self, matches: &ArgMatches) -> reqwest::Result<Response> {
-        unimplemented!()
-    }
-
-    fn put_route_static(&self, matches: &ArgMatches) -> reqwest::Result<Response> {
-        unimplemented!()
-    }
-
-    fn put_routes_static(&self, matches: &ArgMatches) -> reqwest::Result<Response> {
-        unimplemented!()
-    }
-
-    fn put_settlement_engines(&self, matches: &ArgMatches) -> reqwest::Result<Response> {
-        unimplemented!()
-    }
-
-    fn get_root(&self, matches: &ArgMatches) -> reqwest::Result<Response> {
-        unimplemented!()
+    fn get_root(&self, matches: &ArgMatches) -> Result<Response, Error> {
+        self.client
+            .get(&format!("{}/", self.url))
+            .send()
+            .map_err(Error::ClientErr)
     }
 }
 
 // This function takes the map of arguments parsed by Clap
 // and extracts the values for each argument.
-fn extract_args<'a>(matches: &'a ArgMatches) -> std::collections::HashMap<&'a str, &'a str> {
-    matches // Contains data and metadata about the parsed command
+fn extract_args<'a>(matches: &'a ArgMatches) -> (&'a str, HashMap<&'a str, &'a str>) {
+    let mut args: HashMap<_, _> = matches // Contains data and metadata about the parsed command
         .args // The hashmap containing each parameter along with its values and metadata
         .iter()
         .map(|(&key, val)| (key, val.vals[0].to_str().unwrap())) // Extract raw key/value pairs
-        .collect()
+        .collect();
+    let auth = args.remove("authorization_key").unwrap();
+    (auth, args)
 }
--- crates/ilp-cli/src/main.rs
+++ crates/ilp-cli/src/main.rs
@@ -1,10 +1,178 @@
 mod interpreter;
 mod parser;
+use std::process::exit;
 
 pub fn main() {
-    // Define the arguments to the CLI
+    // 1. Define the arguments to the CLI application
     let app = parser::build();
 
-    // Interpret this CLI invocation
-    interpreter::run(app);
+    // 2. Parse the command line
+    let matches = app.clone().get_matches();
+
+    // 3. Interpret this CLI invocation
+    let result = interpreter::run(&matches);
+
+    // 4. Handle interpreter output
+    match result {
+        Err(interpreter::Error::UsageErr(s)) => {
+            // Clap doesn't seem to have a built-in way of manually printing the
+            // help text for an arbitrary subcommand, but this works just the same.
+            app.get_matches_from(s.split(' '));
+        }
+        Err(interpreter::Error::ClientErr(e)) => {
+            eprintln!("ILP CLI error: failed to send request: {}", e);
+            exit(1);
+        }
+        Ok(mut response) => match response.text() {
+            Err(e) => {
+                eprintln!("ILP CLI error: Failed to parse HTTP response: {}", e);
+                exit(1);
+            }
+            Ok(body) => {
+                if response.status().is_success() {
+                    if !matches.is_present("quiet") {
+                        println!("{}", body);
+                    }
+                } else {
+                    eprintln!(
+                        "ILP CLI error: Unexpected response from server: {}: {}",
+                        response.status(),
+                        body,
+                    );
+                    exit(1);
+                }
+            }
+        },
+    }
+}
+
+#[cfg(test)]
+// Note that this module contains interface tests, not integration tests.
+// These exist to detect changes to the parser or interpreter that cause
+// breakage to the tool's user-facing interface.
+// Since this doesn't require a properly configured (or mocked) server,
+// errors from the interpreter are entirely expected, though we still
+// run the interpreter in order to detect panics.
+// Conveniently this section also serves as a reference for example invocations.
+mod interface_tests {
+    use crate::interpreter;
+    use crate::parser;
+
+    #[test]
+    fn ilp_cli() {
+        should_parse(&[
+            "ilp-cli --quiet status",    // quiet
+            "ilp-cli --node bar status", // non-default node
+        ]);
+    }
+
+    #[test]
+    fn accounts_balance() {
+        should_parse(&[
+            "ilp-cli accounts balance alice --auth foo", // minimal
+        ]);
+    }
+
+    #[test]
+    fn accounts_create() {
+        should_parse(&[
+            "ilp-cli accounts create alice --auth foo --asset-code ABC --asset-scale 9", // minimal
+            "ilp-cli accounts create alice --auth foo --asset-code XYZ --asset-scale 6 --ilp-address bar --max-packet-amount 100 --min-balance 0 --ilp-over-http-url qux --ilp-over-http-incoming-token baz --ilp-over-http-outgoing-token qaz --ilp-over-btp-url spam --ilp-over-btp-outgoing-token ham --ilp-over-btp-incoming-token eggs --settle-threshold 0 --settle-to 0 --routing-relation foobar --round-trip-time 1000 --amount-per-minute-limit 42 --packets-per-minute-limit 4 --settlement-engine-url if_you_can_read_this_congratulations_youve_scrolled_too_far_right", // maximal
+            "ilp-cli accounts create alice --auth foo --asset-code ABC --asset-scale 3 --min-balance -1000 --settle-threshold -10", // negative numbers
+        ]);
+    }
+
+    #[test]
+    fn accounts_delete() {
+        should_parse(&[
+            "ilp-cli accounts delete alice --auth foo", // minimal
+        ]);
+    }
+
+    #[test]
+    fn accounts_incoming_payments() {}
+
+    #[test]
+    fn accounts_info() {
+        should_parse(&[
+            "ilp-cli accounts info alice --auth foo", // minimal
+        ]);
+    }
+
+    #[test]
+    fn accounts_list() {
+        should_parse(&[
+            "ilp-cli accounts list --auth foo", // minimal
+        ]);
+    }
+
+    #[test]
+    fn accounts_update() {
+        should_parse(&[
+            "ilp-cli accounts update alice --auth foo", // minimal, non-admin
+            "ilp-cli accounts update alice --auth foo --ilp-over-http-incoming-token bar --ilp-over-btp-incoming-token qux --ilp-over-http-outgoing-token baz --ilp-over-btp-outgoing-token qaz --ilp-over-http-url spam --ilp-over-btp-url eggs --settle-threshold 0 --settle-to 0", // maximal, non-admin
+            "ilp-cli accounts --admin update alice --auth foo", // minimal, admin
+            "ilp-cli accounts --admin update alice --auth foo --asset-code XYZ --asset-scale 6 --ilp-address bar --max-packet-amount 100 --min-balance 0 --ilp-over-http-url qux --ilp-over-http-incoming-token baz --ilp-over-http-outgoing-token qaz --ilp-over-btp-url spam --ilp-over-btp-outgoing-token ham --ilp-over-btp-incoming-token eggs --settle-threshold 0 --settle-to 0 --routing-relation foobar --round-trip-time 1000 --amount-per-minute-limit 42 --packets-per-minute-limit 4 --settlement-engine-url if_you_can_read_this_congratulations_youve_scrolled_too_far_right", // maximal, admin
+            "ilp-cli accounts update alice --auth foo --settle-threshold -1000 --settle-to -10", // negative numbers
+        ]);
+    }
+
+    #[test]
+    fn pay() {
+        should_parse(&[
+            "ilp-cli pay alice --auth foo --source-amount 500 --receiver bar", // minimal
+        ]);
+    }
+
+    #[test]
+    fn rates_list() {
+        should_parse(&[
+            "ilp-cli rates list", // minimal
+        ]);
+    }
+
+    #[test]
+    fn rates_set_all() {
+        should_parse(&["ilp-cli rates set-all --auth foo -r bar 1.0 -r baz 2.0"])
+    }
+
+    #[test]
+    fn routes_list() {
+        should_parse(&[
+            "ilp-cli routes list", // minimal
+        ]);
+    }
+
+    #[test]
+    fn routes_set() {}
+
+    #[test]
+    fn routes_set_all() {
+        should_parse(&["ilp-cli routes set-all --auth foo -r bar qux -r baz qaz"])
+    }
+
+    #[test]
+    fn settlement_engines_set_all() {}
+
+    #[test]
+    fn status() {
+        should_parse(&[
+            "ilp-cli status", // minimal
+        ]);
+    }
+
+    fn should_parse(examples: &[&str]) {
+        let mut app = parser::build();
+        for example in examples {
+            let parser_result = app.get_matches_from_safe_borrow(example.split(' '));
+            match parser_result {
+                Err(e) => panic!("Failure while parsing command `{}`: {}", example, e),
+                Ok(matches) => {
+                    // Any unanticipated errors at this stage will result in a panic from
+                    // within the interpreter, so no need to manually check the result.
+                    let _interpreter_result = interpreter::run(&matches);
+                }
+            }
+        }
+    }
 }
