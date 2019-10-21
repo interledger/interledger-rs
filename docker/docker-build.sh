5f0015c5191de40bceb61a3fca8737240e22da93
Taiga Nakayama <dora@dora-gt.jp>
2019-10-21 10:52:33 +0900

    refactor(scripts): move `docker-build.sh` into `docker` directory
    Signed-off-by: Taiga Nakayama <dora@dora-gt.jp>

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# intended to be run in the top directory
+
 build_targets=()
 
 if [ "$PROFILE" = "release" ]; then
@@ -16,7 +18,6 @@ if [ $# -eq 0 ]; then
     printf "\e[33mNo build target is given, building all targets.\e[m\n"
     build_targets+=(ilp-cli)
     build_targets+=(node)
-    # build_targets+=(settlement-engines)
     build_targets+=(testnet-bundle)
     build_targets+=(circleci-rust-dind)
 fi
@@ -24,23 +25,16 @@ fi
 # if arguments are given, add it as build targets
 build_targets+=($@)
 
-# TODO remove settlement engines
 for build_target in "${build_targets[@]}"; do
     case $build_target in
-        # intended to be run in the top directory
         "ilp-cli") docker build -f ./docker/ilp-cli.dockerfile -t interledgerrs/ilp-cli:latest . ;;
         "node") docker build -f ./docker/node.dockerfile -t interledgerrs/node:latest \
                 --build-arg CARGO_BUILD_OPTION="${CARGO_BUILD_OPTION}" \
                 --build-arg RUST_BIN_DIR_NAME="${RUST_BIN_DIR_NAME}" \
                 . ;;
-        "settlement-engines") docker build -f ./docker/settlement-engines.dockerfile -t interledgerrs/settlement-engines:latest \
-                --build-arg CARGO_BUILD_OPTION="${CARGO_BUILD_OPTION}" \
-                --build-arg RUST_BIN_DIR_NAME="${RUST_BIN_DIR_NAME}" \
-                . ;;
         "testnet-bundle") docker build -f ./docker/Dockerfile -t interledgerrs/testnet-bundle:latest . ;;
+        "circleci-rust-dind") docker build -f ./docker/CI/circleci-rust-dind.dockerfile -t interledgerrs/circleci-rust-dind:latest . ;;
         "ci-node") docker build -f ./docker/CI/node.dockerfile -t interledgerrs/node:latest . ;;
         "ci-ilp-cli") docker build -f ./docker/CI/ilp-cli.dockerfile -t interledgerrs/ilp-cli:latest . ;;
-        "ci-settlement-engines") docker build -f ./docker/CI/settlement-engines.dockerfile -t interledgerrs/settlement-engines:latest . ;;
-        "circleci-rust-dind") docker build -f ./docker/CI/circleci-rust-dind.dockerfile -t interledgerrs/circleci-rust-dind:latest . ;;
     esac
 done
