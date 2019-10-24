6d58fbcfb888db50037dbdcee9e48101f9b8f9e3
Taiga Nakayama <dora@dora-gt.jp>
2019-10-24 20:04:18 +0900

    feat(ci): tag docker images
    Signed-off-by: Taiga Nakayama <dora@dora-gt.jp>

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # intended to be run in the top directory
+docker_image_tag=${DOCKER_IMAGE_TAG:-latest}
 
 build_targets=()
 
@@ -25,16 +26,18 @@ fi
 # if arguments are given, add it as build targets
 build_targets+=($@)
 
+printf "\e[32;1mBuilding docker image with tag: ${docker_image_tag}\e[m\n"
+
 for build_target in "${build_targets[@]}"; do
     case $build_target in
-        "ilp-cli") docker build -f ./docker/ilp-cli.dockerfile -t interledgerrs/ilp-cli:latest . ;;
-        "node") docker build -f ./docker/node.dockerfile -t interledgerrs/node:latest \
+        "ilp-cli") docker build -f ./docker/ilp-cli.dockerfile -t interledgerrs/ilp-cli:${docker_image_tag} . ;;
+        "node") docker build -f ./docker/node.dockerfile -t interledgerrs/node:${docker_image_tag} \
                 --build-arg CARGO_BUILD_OPTION="${CARGO_BUILD_OPTION}" \
                 --build-arg RUST_BIN_DIR_NAME="${RUST_BIN_DIR_NAME}" \
                 . ;;
-        "testnet-bundle") docker build -f ./docker/Dockerfile -t interledgerrs/testnet-bundle:latest . ;;
-        "circleci-rust-dind") docker build -f ./docker/CI/circleci-rust-dind.dockerfile -t interledgerrs/circleci-rust-dind:latest . ;;
-        "ci-node") docker build -f ./docker/CI/node.dockerfile -t interledgerrs/node:latest . ;;
-        "ci-ilp-cli") docker build -f ./docker/CI/ilp-cli.dockerfile -t interledgerrs/ilp-cli:latest . ;;
+        "testnet-bundle") docker build -f ./docker/Dockerfile -t interledgerrs/testnet-bundle:${docker_image_tag} . ;;
+        "circleci-rust-dind") docker build -f ./docker/CI/circleci-rust-dind.dockerfile -t interledgerrs/circleci-rust-dind:${docker_image_tag} . ;;
+        "ci-node") docker build -f ./docker/CI/node.dockerfile -t interledgerrs/node:${docker_image_tag} . ;;
+        "ci-ilp-cli") docker build -f ./docker/CI/ilp-cli.dockerfile -t interledgerrs/ilp-cli:${docker_image_tag} . ;;
     esac
 done
