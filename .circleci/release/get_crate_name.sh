9fb4f6578a1bf9087ea3f276fa0638090fffa0ac
Taiga Nakayama <dora@dora-gt.jp>
2019-10-26 02:34:48 +0900

    ci(ci): publish binaries when tags are pushed
    Signed-off-by: Taiga Nakayama <dora@dora-gt.jp>

---
+++ .circleci/release/get_crate_name.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Returns a crate name to build from a git tag name.
+# The tag name is assumed to be output by `cargo release` or be tagged manually.
+
+if [[ $1 =~ ^(.*)-v([0-9]+)\.([0-9]+)\.([0-9]+)(-([0-9A-Za-z-]+(\.[0-9A-Za-z-]+)*))?(\+[0-9A-Za-z-]+)?$ ]]; then
+    echo ${BASH_REMATCH[1]}
+elif [[ $1 =~ ^ilp-node-.*$ ]]; then
+    echo "ilp-node"
+elif [[ $1 =~ ^ilp-cli-.*$ ]]; then
+    echo "ilp-cli"
+fi
