cf9eaeba5d764d49b172419663c4e242fafbf579
Georgios Konstantopoulos <me@gakonst.com>
2019-11-05 13:05:59 +0200

    fix(settlement): export Redis and API only if explicitly asked

--- crates/interledger-store-redis/src/store.rs
+++ crates/interledger-store-redis/src/store.rs
@@ -32,16 +32,14 @@ use http::StatusCode;
 use interledger_api::{AccountDetails, AccountSettings, EncryptedAccountSettings, NodeStore};
 use interledger_btp::BtpStore;
 use interledger_ccp::{CcpRoutingAccount, RouteManagerStore, RoutingRelation};
-use interledger_http::{
-    HttpStore,
-};
+use interledger_http::HttpStore;
 use interledger_packet::Address;
 use interledger_router::RouterStore;
 use interledger_service::{Account as AccountTrait, AccountStore, AddressStore, Username};
 use interledger_service_util::{BalanceStore, ExchangeRateStore, RateLimitError, RateLimitStore};
 use interledger_settlement::settlement_core::{
-    scale_with_precision_loss, 
     idempotency::{IdempotentData, IdempotentStore},
+    scale_with_precision_loss,
     types::{Convert, ConvertDetails, LeftoversStore, SettlementStore},
 };
 use interledger_stream::{PaymentNotification, StreamNotificationsStore};
