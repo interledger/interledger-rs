cc8f40b6fc691cdc3b1b151237abf07be1b985fc
Georgios Konstantopoulos <me@gakonst.com>
2019-11-09 11:41:56 +0200

    fix(settlement): rename api.rs files to be explicit about which API they refer to

--- crates/interledger-settlement/src/settlement_api/mod.rs
+++ crates/interledger-settlement/src/settlement_api/mod.rs
@@ -1,6 +1,6 @@
-mod api;
 mod client;
 mod message_service;
+mod node_api;
 
 #[cfg(test)]
 mod fixtures;
@@ -8,6 +8,6 @@ mod fixtures;
 mod test_helpers;
 
 // Expose the API creation filter method and the necessary services
-pub use api::create_settlements_filter;
 pub use client::SettlementClient;
 pub use message_service::SettlementMessageService;
+pub use node_api::create_settlements_filter;
