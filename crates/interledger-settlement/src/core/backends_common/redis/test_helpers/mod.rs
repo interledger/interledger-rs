44f65a1e83756d6a1ebebb7cb2aaa1f57e281e2f
Georgios Konstantopoulos <me@gakonst.com>
2020-01-07 14:38:31 +0200

    feat(settlement): move to futures 0.3 and async/await

--- crates/interledger-settlement/src/core/backends_common/redis/test_helpers/mod.rs
+++ crates/interledger-settlement/src/core/backends_common/redis/test_helpers/mod.rs
@@ -3,4 +3,4 @@ mod redis_helpers;
 #[cfg(test)]
 mod store_helpers;
 #[cfg(test)]
-pub use store_helpers::{block_on, test_store, IDEMPOTENCY_KEY};
+pub use store_helpers::{test_store, IDEMPOTENCY_KEY};
