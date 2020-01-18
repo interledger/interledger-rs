dbc308c1c944c6f69225e287bb1adef9001fe279
Georgios Konstantopoulos <me@gakonst.com>
2020-01-18 12:28:07 +0200

    fix: bump ilp-node type-length-limit

--- crates/ilp-node/src/lib.rs
+++ crates/ilp-node/src/lib.rs
@@ -1,5 +1,6 @@
 // #![type_length_limit = "25000000"]
-#![type_length_limit = "1500000"] // needed to cargo build --bin ilp-node --feature "monitoring"
+// #![type_length_limit = "1500000"] // needed to cargo build --bin ilp-node --feature "monitoring"
+#![type_length_limit = "40000000"] // needed to cargo build --all-features --all-targets
 
 mod instrumentation;
 mod node;
--- crates/ilp-node/tests/redis/redis_tests.rs
+++ crates/ilp-node/tests/redis/redis_tests.rs
@@ -1,4 +1,5 @@
-#![type_length_limit = "5000000"] // this is needed for cargo test
+// #![type_length_limit = "5000000"] // this is needed for cargo test
+#![type_length_limit = "40000000"] // this is needed for cargo test --all-features --all
 
 mod btp;
 mod exchange_rates;
