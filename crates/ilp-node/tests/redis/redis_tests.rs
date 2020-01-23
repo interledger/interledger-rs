f18022d281c2d8ece1cb69bc78f7a4f1b38d2fdf
Georgios Konstantopoulos <me@gakonst.com>
2020-01-23 13:02:26 +0200

    test(ilp-node): migrate tests to futures 0.3

--- crates/ilp-node/tests/redis/redis_tests.rs
+++ crates/ilp-node/tests/redis/redis_tests.rs
@@ -1,7 +1,13 @@
+// #![type_length_limit = "5000000"] // this is needed for cargo test
+#![type_length_limit = "40000000"] // this is needed for cargo test --all-features --all
+
 mod btp;
 mod exchange_rates;
-mod prometheus;
 mod three_nodes;
+
+// Only run prometheus tests if the monitoring feature is turned on
+#[cfg(feature = "monitoring")]
+mod prometheus;
 
 mod redis_helpers;
 mod test_helpers;
