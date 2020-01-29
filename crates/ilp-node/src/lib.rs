26734f97bba9be5efbbff7fc756377deee8cd01b
Georgios Konstantopoulos <me@gakonst.com>
2020-01-29 10:21:19 +0200

    [Part 13/N] Interledger Store: Futures 0.3 Transition (#606)
    * feat(store): Update redis reconnect
    
    * feat(store): Update base redis struct
    
    * feat(store): Update AccountStore trait
    
    * feat(store): Update StreamNotificationsStore trait
    
    * feat(store): Update BalanceStore trait
    
    * feat(store): Update BtpStore trait
    
    * feat(store): Update HttpStore trait
    
    * feat(store): Update NodeStore trait
    
    * feat(store): Update AddressStore trait
    
    * feat(store): Update RouteManagerStore trait
    
    * feat(store): Update RateLimitStore trait
    
    * feat(store): Update IdempotentStore trait
    
    * feat(store): Update SettlementStore trait
    
    * feat(store): Update LeftoversStore trait
    
    * feat(store): Update update_routes
    
    * test(store): convert all tests to tokio::test with async/await
    
    * feat(store): update secrecy/bytes/zeroize
    
    * docs(store): add more docs
    
    # ILP CLI: Futures 0.3 Transition (#607)
    
    * feat(ilp-cli): update CLI to async/await
    
    # ILP Node: Futures 0.3 Transition (#608) (#609)
    
    * test(ilp-node): migrate tests to futures 0.3
    
    * feat(ilp-node): move metrics related files to feature-gated module
    
    * feat(ilp-node): remove deprecated insert account function
    
    * feat(ilp-node): make the node run on async/await
    
    * ci(ilp-node): disable some advisories and update README
    
    * fix(ilp-node): spawn prometheus filter
    
    # Service 
    * feat(service): Box wrapper methods to avoid exponential type blowup

--- crates/ilp-node/src/lib.rs
+++ crates/ilp-node/src/lib.rs
@@ -1,15 +1,8 @@
-#![type_length_limit = "1152909"]
-
-mod metrics;
+#![type_length_limit = "5000000"]
+mod instrumentation;
 mod node;
-mod trace;
 
-#[cfg(feature = "google-pubsub")]
-mod google_pubsub;
 #[cfg(feature = "redis")]
 mod redis_store;
 
 pub use node::*;
-#[allow(deprecated)]
-#[cfg(feature = "redis")]
-pub use redis_store::insert_account_with_redis_store;
--- crates/interledger-store/src/lib.rs
+++ crates/interledger-store/src/lib.rs
@@ -2,7 +2,10 @@
 //!
 //! Backend databases for storing account details, balances, the routing table, etc.
 
+/// A module to define the primitive `Account` struct which implements `Account` related traits.
 pub mod account;
+/// Cryptographic utilities for encrypting/decrypting data as well as clearing data from memory
 pub mod crypto;
+/// A redis backend using [redis-rs](https://github.com/mitsuhiko/redis-rs/)
 #[cfg(feature = "redis")]
 pub mod redis;
