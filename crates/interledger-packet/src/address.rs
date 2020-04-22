b1fcbebd2dbbf27988091f5f3c0916847a949ff5
KristianWahlroos <kristian.whl@gmail.com>
2020-04-22 22:38:17 +0300

    fix: AddressError Display impl (#638)
    * Replace deprecated description with Display impl
    
    * Refactor to use deref in stream's client
    
    * Allow clippy's mutable key type in test helpers
    
    * Run cargo update
    
    * Make AddressError use thiserror

--- crates/interledger-packet/src/address.rs
+++ crates/interledger-packet/src/address.rs
@@ -18,31 +18,17 @@ use once_cell::sync::Lazy;
 
 const MAX_ADDRESS_LENGTH: usize = 1023;
 
-#[derive(Debug)]
+#[derive(thiserror::Error, Debug)]
 pub enum AddressError {
+    #[error("Invalid address length: {0}")]
     InvalidLength(usize),
+    #[error("Invalid address format")]
     InvalidFormat,
 }
 
 static ADDRESS_PATTERN: Lazy<Regex> = Lazy::new(|| {
     Regex::new(r"^(g|private|example|peer|self|test[1-3]?|local)([.][a-zA-Z0-9_~-]+)+$").unwrap()
 });
-
-use std::error::Error;
-impl Error for AddressError {
-    fn description(&self) -> &str {
-        match *self {
-            AddressError::InvalidLength(_length) => "Invalid address length",
-            AddressError::InvalidFormat => "Invalid address format",
-        }
-    }
-}
-
-impl fmt::Display for AddressError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        self.description().fmt(f)
-    }
-}
 
 /// An ILP address backed by `Bytes`.
 #[derive(Clone, Eq, Hash, PartialEq)]
