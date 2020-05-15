2d7ded580af8e73a5935b6dd3ef7fe6c1295f187
ljedrz <ljedrz@gmail.com>
2020-05-15 15:35:53 +0200

    perf: remove a few redundant clones
    Signed-off-by: ljedrz <ljedrz@gmail.com>

--- crates/ilp-node/src/main.rs
+++ crates/ilp-node/src/main.rs
@@ -135,7 +135,7 @@ async fn main() {
         }
         set_app_env(&config, &mut app, &path, path.len());
     }
-    let matches = app.clone().get_matches();
+    let matches = app.get_matches();
     merge_args(&mut config, &matches);
 
     let node = config
