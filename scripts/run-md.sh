e67b5cc87a5d557f3a3fe1c061071fa4cc64912a
Petar Radovic <petar.radovic@gmail.com>
2021-04-02 18:20:20 +0200

    ci: upload tcpdump along with logs in test-md.

--- scripts/run-md.sh
+++ scripts/run-md.sh
@@ -15,9 +15,16 @@ else
   MD_FILE=-
 fi
 
-cat "$MD_FILE" | "$(dirname $0)/parse-md.sh" > "$TMP_SCRIPT"
-bash -O expand_aliases "$TMP_SCRIPT"
+# run tcpdump in the same directory where other artifacts to be uploaded reside
+TCPDUMP_OUTPUT_FILENAME=$(echo "$MD_FILE" | sha256sum | cut -f1 -d\ ).pcap
+echo "saving packet capture for '$MD_FILE' as $TCP_DUMP_OUTPUT_FILENAME"
+sudo tcpdump -i lo -s 65535 -w "/tmp/run-md-test/${TCPDUMP_OUTPUT_FILENAME}" &
+TCPDUMP_PID=$!
 
+cat "$MD_FILE" | "$(dirname $0)/parse-md.sh" > "$TMP_SCRIPT"
+bash -x -O expand_aliases "$TMP_SCRIPT"
+
+sudo kill -2 $TCPDUMP_PID
 if [ $? -eq 0 ]; then
   rm "$TMP_SCRIPT"
   exit 0
